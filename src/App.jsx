561c1cf578ef5479895b9f4730b551a1e7b8e696
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-22 12:42:07 +0200

    loading intro init

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { CustomerReviews, Footer, SpecialOffer, Services, Subscribe, SuperQuality, PopularProducts, Hero } from './sections';
 import Nav from './components/Nav';
 
-const App = () => (
 
-    <main className='relative'>
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const spinner = document.getElementById("loader-overlay");
+
+    if (spinner) {
+      setTimeout(() => {
+        spinner.classList.add("loader-overlay-hidden");
+        setLoading(false);
+      }, 1000000);
+    }
+  }, []);
+
+  return (
+    !loading && (
+      <main className='relative'>
         <Nav />
         <section className='xl:padding-1 wide:padding-r padding-b'>
-            <Hero />
+          <Hero />
         </section>
         <section className='padding'>
-            <PopularProducts />
+          <PopularProducts />
         </section>
         <section className='padding'>
-            <SuperQuality />
+          <SuperQuality />
         </section>
         <section className='padding-x py-10'>
-            <Services />
+          <Services />
         </section>
         <section className='padding'>
-            <SpecialOffer />
+          <SpecialOffer />
         </section>
         <section className='padding bg-pale-blue'>
-            <CustomerReviews />
+          <CustomerReviews />
         </section>
         <section className='padding-x sm:py-32 py-16 w-full'>
-            <Subscribe />
+          <Subscribe />
         </section>
         <section className='bg-black padding-x padding-t pb-8'>
-            <Footer />
+          <Footer />
         </section>
-    </main>
-);
+      </main>
+    )
+  );
+};
 
-export default App
+export default App;
