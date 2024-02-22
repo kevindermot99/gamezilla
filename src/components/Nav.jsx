2baccf2ac7d6683a1907cc3acde84b8743c62b50
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-22 09:23:50 +0200

    navbar complete

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import HeaderLogo from "../assets/images/favicon.png"
 import { CgMenuRight } from "react-icons/cg";
 import { navLinks } from "../constants";
 import { bannerLinks } from "../constants";
-import { IoMdHeartEmpty } from "react-icons/io";
+import { GoHeart } from "react-icons/go";
 import { BsCart3 } from "react-icons/bs";
 import { RiSearch2Line } from "react-icons/ri";
 
@@ -13,15 +13,14 @@ function Nav() {
                 {bannerLinks.map((item) => (
                     <a href={item.href} className="banner-link">{item.label}</a>
                 ))}
-                
-                
+
+
             </div>
             <nav className="flex justify-between items-center max-container padding-x py-3">
-                <a href="/" className="flex justify-start items-center gap-2 text-base font-semibold outline-none">
+                <a href="/" className="flex justify-start items-center gap-2 text-base font-medium outline-none">
                     <img src={HeaderLogo}
                         alt="logo"
-                        height={32}
-                        width={32}
+                        className="text-black w-7 h-7"
                     />
                     <span className="text-black">Selected.</span>
                 </a>
@@ -30,7 +29,7 @@ function Nav() {
                         <li key={item.label}>
                             <a
                                 href={item.href}
-                                className=" outline-none leading-normal text-sm font-semibold text-black transition hover:text-blue-600"
+                                className=" outline-none leading-normal text-sm font-medium text-black transition hover:text-blue-600"
                             >
                                 {item.label}
                             </a>
@@ -41,11 +40,17 @@ function Nav() {
 
                 <div className="flex justify-center items-center gap-3">
                     <label className="relative">
-                        <input className="search" type="search" placeholder="Search.."/>
+                        <input className="search" type="search" placeholder="Search.." />
                         <RiSearch2Line className="absolute top-0 bottom-0 m-auto left-4 size-5 text-slate-400" />
                     </label>
-                </div>
 
+                    <GoHeart className="h-5 w-5 mx-2 cursor-pointer transition hover:text-blue-600" />
+                    <a href="#" className="relative transition hover:text-blue-600">
+                        <BsCart3 className="h-5 w-5 cursor-pointer relative" />
+                        <span className="absolute -right-2 -top-2 bg-red-600 font-normal rounded-full leading-normal h-5 w-5 text-xs flex justify-center items-center text-white">2</span>
+                    </a>
+
+                </div>
             </nav>
         </header>
     )
