9ad758485eabbb31a2a2366d413296f4a05e2c8d
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-24 01:44:49 +0200

    navbar bg class

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import HeaderLogo from "../assets/images/logo.png"
 import { CgMenuRight } from "react-icons/cg";
 import { navLinks } from "../constants";
 import { bannerLinks } from "../constants";
-import { VscLibrary } from "react-icons/vsc";
+import { BiBookAlt } from "react-icons/bi";
 import { RiUser6Line } from "react-icons/ri";
 import { RiSearch2Line } from "react-icons/ri";
 import { GrClose } from "react-icons/gr";
@@ -27,14 +27,14 @@ function Nav() {
 
     return (
 
-        <header className="font-montserrat w-ful bg-[#0d0b20] ">
+        <header className="font-montserrat w-ful bg-default ">
             <div className="flex justify-end items-center h-9 text-white padding-x py-2 gap-4 max-lg:px-3 max-lg:pr-5">
                 {bannerLinks.map((item) => (
                     <a href={item.href} className="banner-link text-white">{item.label}</a>
                 ))}
             </div>
             <Headroom pinStart={36}>
-                <nav className="flex justify-between items-center max-container padding-x max-lg:px-3 max-lg:pr-5 py-3">
+                <nav className="flex justify-between items-center max-container padding-x max-lg:px-3 max-lg:pr-5 py-3 bg-default">
                     <a href="/" className="flex justify-start items-center gap-2 text-base font-medium">
                         <img src={HeaderLogo}
                             alt="logo"
@@ -63,17 +63,17 @@ function Nav() {
 
 
                     <div className="flex justify-center items-center gap-3">
-                        <label className={`relative responsive-search ${isMini ? "mini" : ""} max-sm:hidden `}>
+                        <label className={`relative responsive-search ${isMini ? "mini" : ""} max-sm:hidden max-lg:mr-3 `}>
                             <input className="search" type="search" placeholder="Search.." />
                             <RiSearch2Line className="absolute top-0 bottom-0 m-auto left-4 size-5 text-slate-400" />
                         </label>
 
                         <a href="#" className="relative transition hover:text-blue-400 max-lg:mx-4">
-                            <VscLibrary className="h-5 w-5 mx-2 max-lg:mx-0 cursor-pointer relative text-white" />
+                            <BiBookAlt className="h-5 w-5 mx-2 max-lg:mx-0 cursor-pointer relative text-slate-400 hover:text-white" />
                             <span className="absolute -right-0 max-lg:-right-2 -top-2 bg-red-600 font-normal rounded-full leading-normal h-4 w-4 text-xs flex justify-center items-center text-white">2</span>
                         </a>
                         <a href="#" className="relative transition hover:text-blue-400 max-lg:mx-4">
-                            <RiUser6Line className="h-5 w-5 cursor-pointer relative text-white" />
+                            <RiUser6Line className="h-5 w-5 cursor-pointer relative text-slate-400 hover:text-white" />
                         </a>
 
                         <CgMenuRight onClick={toggleMenu} className="text-white ml-2 text-2xl hidden max-lg:block cursor-pointer transition hover:text-blue-600" />
