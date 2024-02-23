b0a64ec36389de0ef62463a2ff10d4028cb2cfcf
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-23 14:27:48 +0200

    Nike commercial images

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,9 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import bg1 from "../assets/images/1.jpg"
+import bg2 from "../assets/images/2.jpg"
 
 function Hero() {
   return (
     <section id="#home" className="bg-grey-50 bg-white min-h-96">
-     <h1>hello</h1>
     </section>
   )
 }
