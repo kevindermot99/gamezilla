04f386d154219e7f800b276fa5fd915c9c1b6bc7
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-24 09:29:03 +0200

    home content init

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,17 @@ import bg2 from "../assets/images/2.jpg"
 
 function Hero() {
   return (
-    <section id="#home" className="bg-grey-50 bg-white min-h-96">
+    <section id="#home" className="bg-grey-50 bg-default grid grid-cols-2 ">
+      <div>
+        <h4 className=""></h4>
+        <h1>Book Title</h1>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita maiores explicabo quis pariatur illum velit totam optio error natus repudiandae, doloribus ab perspiciatis nemo, id aspernatur reprehenderit impedit neque illo.</h3>
+        <button>Read</button>
+      </div>
+      <div>
+        <img src="https://bukovero.com/wp-content/uploads/2016/07/Harry_Potter_and_the_Cursed_Child_Special_Rehearsal_Edition_Book_Cover.jpg" />
+      </div>
+
     </section>
   )
 }
