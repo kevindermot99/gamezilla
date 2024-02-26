bddd6b0e684224266e48eb42623bb2f5cfb2bc43
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-26 19:35:35 +0200

    notification color decision

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Notification = ({label}) => {
   return (
-    <div className="notification bg-main-color bg-red-600  ">
+    <div className="notification bg-red-600  ">
         {label}
     </div>
   )
