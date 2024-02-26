ba3816e80abdbe319f0d6bfa1a082181c7f5684a
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-26 22:34:15 +0200

    full screen icon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,6 +6,7 @@ import Headroom from "react-headroom";
 import Notification from "./Notification";
 import { IoLanguage } from "react-icons/io5";
 import { TbMoonStars } from "react-icons/tb";
+import { RxEnterFullScreen } from "react-icons/rx";
 
 function Nav() {
 
@@ -15,7 +16,7 @@ function Nav() {
 
         <header className="font-montserrat w-ful">
             {/* <Headroom pinStart={0}> */}
-            <nav className="flex justify-between items-center max-container px-[2.5%] py-4 bg-white sticky top-0 z-index-10">
+            <nav className="flex justify-between items-center max-container px-[2.5%] py-5 bg-white sticky top-0 z-index-10">
                 <a href="/" className="flex justify-start items-center gap-2 text-base font-medium">
                     <img src={pagesLogo} alt="logo" className=" h-9" />
                     <h1 className="text-black font-medium text-[19px] font-DMsans">Pagez</h1>
@@ -23,22 +24,26 @@ function Nav() {
 
 
 
-                <div className="flex w-fit justify-end items-center gap-3 ">
+                <div className="flex w-fit justify-end items-center gap-3    ">
 
                     <a href="#" className="relative transition rounded-md hover:bg-gray-100 p-2">
-                        <TbMoonStars className="h-5 w-5 cursor-pointer relative transition text-black" />
+                        <RxEnterFullScreen className=" text-xl cursor-pointer relative transition text-black" />
                     </a>
 
                     <a href="#" className="relative transition rounded-md hover:bg-gray-100 p-2">
-                        <IoLanguage className="h-5 w-5 cursor-pointer relative transition text-black" />
+                        <TbMoonStars className="text-xl cursor-pointer relative transition text-black" />
                     </a>
 
                     <a href="#" className="relative transition rounded-md hover:bg-gray-100 p-2">
-                        <BiBookAlt className="h-5 w-5 cursor-pointer relative transition text-black" />
-                        {/* <Notification label={9} /> */}
+                        <IoLanguage className="text-xl cursor-pointer relative transition text-black" />
+                    </a>
+
+                    <a href="#" className="relative transition rounded-md hover:bg-gray-100 p-2">
+                        <BiBookAlt className="text-xl cursor-pointer relative transition text-black" />
+                        <Notification label={9} />
                     </a>
                     <a href="#" className="relative transition rounded-md hover:bg-gray-100 p-2">
-                        <RiUser6Line className="h-5 w-5 cursor-pointer relative transition text-black" />
+                        <RiUser6Line className="text-xl cursor-pointer relative transition text-black" />
                     </a>
 
                 </div>
