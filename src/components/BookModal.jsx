f9e5a682ad3d0e513427ed8f9c58d410aa26f4a4
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-26 19:39:14 +0200

    Repo Renamed

--- src/components/BookModal.jsx
+++ src/components/BookModal.jsx
@@ -10,7 +10,7 @@ function BookModal({ selectedBook, closeModal }) {
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 w-full h-[100vh] bg-black bg-opacity-80 overscroll-contain z-50 flex items-end justify-center'>
       <div className='bg-white h-[85%] w-[100%] max-w-[900px] rounded-t-3xl p-6'>
-        <div className=' h-full w-full overflow-y-auto overscroll-contain flex flex-col'>
+        <div className=' h-full w-full overflow-y-auto overscroll-contain flex flex-col read-only:'>
           <MdCancel className=' self-end text-gray-300 transit hover:text-main-color cursor-pointer text-3xl' onClick={closeModal}/>
         </div>
       </div>
