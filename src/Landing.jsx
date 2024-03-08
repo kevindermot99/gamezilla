b9fb2a5de9ef51055ca1a0805c57c8eb8b5a05dd
kevindermot99 <mbonimpayekevin@gmail.com>
2024-03-08 10:06:19 +0200

    Landing page combined

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import LandingNavBar from './LandingNavBar'
+import { Link } from 'react-router-dom'
+import pagesLogo from "./assets/images/pagez-logo.png"
 
 function Landing() {
   return (
-    <div >
-      <LandingNavBar />
+    <div className='px-14 '>
+      <div className="flex justify-between items-center py-7">
+            <div className='flex justify-center items-center gap-5'>
+                <Link to="/" className='flex justify-start items-center gap-2 text-base font-medium mr-4'>
+                    <img src={pagesLogo} alt="Logo" className='h-9' />
+                    <p className='linkText'>Pagez</p>
+                </Link>
+                <Link to="/" className='linkText'>Overview</Link>
+                <Link className='linkText'>Features</Link>
+                <Link className='linkText'>Creator</Link>
+                <Link className='linkText'>Source code</Link>
+            </div>
+            <div>
+                
+            </div>
+
+        </div>
       <h1>Landing</h1>
     </div>
   )
