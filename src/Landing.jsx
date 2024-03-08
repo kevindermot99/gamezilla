8ce2dad72d6ce98cbfb329197f64de7857c9d477
kevindermot99 <mbonimpayekevin@gmail.com>
2024-03-08 13:06:20 +0200

    screenshot animation

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -26,8 +26,8 @@ function Landing() {
       </nav>
       <section className=' flex flex-col items-center justify-center text-center p-7 relative'>
 
-        <div className="absolute top-56 left-7 -z-[10] size-[700px] blur-3xl opacity-15 rounded-[50%] bg-main-color"></div>
-        <div className="absolute -bottom-10 right-7 -z-[10] size-[700px] blur-3xl opacity-15 rounded-[50%] bg-main-color"></div>
+        <div className="absolute top-56 left-7 -z-[10] size-[700px] blur-3xl opacity-10 rounded-[50%] bg-main-color"></div>
+        <div className="absolute -bottom-10 right-7 -z-[10] size-[700px] blur-3xl opacity-10 rounded-[50%] bg-main-color"></div>
 
         <h1 className='capitalize font-semibold text-[70px] text-black text-wrap max-w-[820px] leading-[80px] mt-[50px]'>
           dive into The <span className='text-main-color '>ocean</span> of Free novels
@@ -35,8 +35,8 @@ function Landing() {
         <p className='text-base  font-normal leading-6 max-w-[800px] text-black text-wrap m-[40px]'>
           Read or Download Novels online at $0.00 cost, Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptate at necessitatibus sed dolorum perferendis in Voluptas, deleniti?
         </p>
-        <div>
-          <img src={screenShoot} className=' w-full max-w-[1000px] shadow-xl rounded-xl mt-4' alt="screenshot" />
+        <div className='gradient-moving-bg shadow-xl'>
+          <img src={screenShoot} className=' w-full max-w-[1000px] rounded-xl' alt="screenshot" />
         </div>
       </section>
     </div>
