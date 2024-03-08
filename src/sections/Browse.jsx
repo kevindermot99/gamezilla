3851ab41b3adfcf3af7ce744a8bff7dd4f256256
kevindermot99 <mbonimpayekevin@gmail.com>
2024-03-08 08:48:54 +0200

    filter updates

--- src/sections/Browse.jsx
+++ src/sections/Browse.jsx
@@ -47,35 +47,39 @@ function Browse() {
   }, [])
 
   return (
-    <section className=' flex flex-col gap-5 '>
-      <Search />
+    <section className=' flex gap-5 '>
+      <div className='bg-blue-300 w-[30%] max-w-[250px]'>
+      </div>
+      <div className='flex flex-col gap-5 w-full'>
+        <Search />
 
-      <div className=' flex justify-start items-start max-sm:gap-0 relative'>
-        <div className='h-fit w-full '>
-          <div className='book-grid w-fit max-sm:mt-0 mt-2 '>
-            {Books.map((book) => (
-              <div className='relative object-cover flex-grow '>
-                <div className='relative '>
-                  <img src={book.URL} loading='lazy' key={book.id} alt={book.id} className=' h-[200px] max-sm:h-[240px] w-full m-auto object-cover flex-1 rounded-sm drop-shadow-lg ring-1 ring-gray-100 ' />
-                  <div className='absolute h-full w-full bg-slate-900 bg-opacity-70 transition z-1 top-0 flex justify-center items-center flex-col gap-2 opacity-0 hover:opacity-100'>
-                    <a href="#" className='h-9 w-24 flex justify-center items-center capitalize font-medium text-sm cursor-pointer bg-main-color text-white py-2 px-4 rounded-md transition'>open</a>
-                    <button className='h-9 w-24 text-sm font-medium bg-white text-black capitalize rounded-md ' onClick={() => openModal(book)}>details</button>
+        <div className=' flex justify-start items-start max-sm:gap-0 relative'>
+          <div className='h-fit w-full '>
+            <div className='book-grid w-fit max-sm:mt-0 mt-2 '>
+              {Books.map((book) => (
+                <div className='relative object-cover flex-grow '>
+                  <div className='relative '>
+                    <img src={book.URL} loading='lazy' key={book.id} alt={book.id} className=' h-[200px] max-sm:h-[240px] w-full m-auto object-cover flex-1 rounded-sm drop-shadow-lg ring-1 ring-gray-100 ' />
+                    <div className='absolute h-full w-full bg-slate-900 bg-opacity-70 transition z-1 top-0 flex justify-center items-center flex-col gap-2 opacity-0 hover:opacity-100'>
+                      <a href="#" className='h-9 w-24 flex justify-center items-center capitalize font-medium text-sm cursor-pointer bg-main-color text-white py-2 px-4 rounded-md transition'>open</a>
+                      <button className='h-9 w-24 text-sm font-medium bg-white text-black capitalize rounded-md ' onClick={() => openModal(book)}>details</button>
+                    </div>
                   </div>
-                </div>
-                <p className='h-fit font-medium text-sm pt-3 pb-3 text-center'>
-                  {book.title}
-                </p>
+                  <p className='h-fit font-medium text-sm pt-3 pb-3 text-center'>
+                    {book.title}
+                  </p>
 
-              </div>
-            ))}
+                </div>
+              ))}
+
+            </div>
 
           </div>
-
         </div>
       </div>
 
       {/* modal */}
-      <BookModal selectedBook={selectedBook} closeModal={closeModal}/>
+      <BookModal selectedBook={selectedBook} closeModal={closeModal} />
     </section>
   )
 }
