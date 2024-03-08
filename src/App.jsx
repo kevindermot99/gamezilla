cfad3b12000fbd09bdef91cde2e3e13c598106cc
kevindermot99 <mbonimpayekevin@gmail.com>
2024-03-08 09:33:34 +0200

    landing page init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './Landing';
+
+
 
 const App = () => {
-
   return (
     // !loading && (
-      <></>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
     // )
   );
 };
