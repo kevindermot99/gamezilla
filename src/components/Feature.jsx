394f3800848f53b0b6b62a70108191703cf5636f
kevindermot99 <mbonimpayekevin@gmail.com>
2024-03-12 10:54:17 +0200

    feature bg complete

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -12,7 +12,7 @@ function Feature({tip, title, description, URL, direction, position}) {
             <p className='text-gray-600 font-medium text-xl leading-relaxed '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis accusamus officiis porro. Cupiditate corporis odit pariatur sunt! Inventore iusto, expedita eos aut minima ipsum! Minus quia totam tempora blanditiis vel.</p>
             <p className='text-main-color font-extrabold '>#{position}</p>        
         </div>
-        <div className={`w-[45%] h-[350px] ${position == '1' && 'bg-orange-200 '} ${position == '2' && 'bg-violet-300 '} ${position == '3' && 'bg-rose-200  '}  relative overflow-clip rounded-2xl pointer-events-none shadow-xl`}>
+        <div className={`w-[45%] h-[350px] ${position == '1' && 'bg-orange-200 '} ${position == '2' && 'bg-violet-400 '} ${position == '3' && 'bg-rose-400  '}  relative overflow-clip rounded-2xl pointer-events-none shadow-xl`}>
             <video src={URL} autoPlay muted loop className='absolute h-[90%] w-[90%] object-cover -bottom-[10px] right-0 rounded-tl-2xl' />
         </div>
     </div>
