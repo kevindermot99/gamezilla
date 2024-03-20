61ee5ad2496f6e7aa86e5a1fe0da571330813982
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-20 11:06:39 +0200

    sidebar links with icons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import pagesLogo from "../assets/images/pagez-logo.png"
 import { TbMenu2 } from "react-icons/tb";
+import { GoHomeFill } from "react-icons/go";
+import { Link } from 'react-router-dom';
 
 
 function SideBar() {
     return (
-        <div className="min-w-[240px] h-[100vh] flex flex-col items-start justify-start p-3 ">
-            <div className="flex justify-start items-center gap-2 mb-9">
+        <div className="min-w-[240px] h-[100vh] flex flex-col items-start justify-start p-3 border-r border-gray-800 ">
+            <div className="flex justify-start items-center gap-2 mb-5 px-2">
                 <button className="h-[40px] w-[40px] transition hover:bg-gray-800 flex items-center justify-center rounded-full">
                     <TbMenu2 className="text-2xl text-white" />
                 </button>
@@ -15,8 +17,31 @@ function SideBar() {
                     <h1 className="logoText">Pagez</h1>
                 </a>
             </div>
-            <ul>
-                
+            <ul className='w-full h-full overflow-y-auto'>
+                <Link className=' flex items-center justify-start text-[15px] gap-5 outline-none w-full px-4 py-3 transition rounded-lg  hover:bg-gray-800 '>
+                    <GoHomeFill  className='text-[23px] '/>
+                    <p className='font-light  '>Home</p>
+                </Link>
+
+                <Link className=' flex items-center justify-start text-[15px] gap-5 outline-none w-full px-4 py-3 transition rounded-lg  hover:bg-gray-800 '>
+                    <GoHomeFill  className='text-[23px] '/>
+                    <p className='font-light  '>Home</p>
+                </Link>
+
+                <Link className=' flex items-center justify-start text-[15px] gap-5 outline-none w-full px-4 py-3 transition rounded-lg  hover:bg-gray-800 '>
+                    <GoHomeFill  className='text-[23px] '/>
+                    <p className='font-light  '>Home</p>
+                </Link>
+
+                <Link className=' flex items-center justify-start text-[15px] gap-5 outline-none w-full px-4 py-3 transition rounded-lg  hover:bg-gray-800 '>
+                    <GoHomeFill  className='text-[23px] '/>
+                    <p className='font-light  '>Home</p>
+                </Link>
+
+                <Link className=' flex items-center justify-start text-[15px] gap-5 outline-none w-full px-4 py-3 transition rounded-lg  hover:bg-gray-800 '>
+                    <GoHomeFill  className='text-[23px] '/>
+                    <p className='font-light  '>Home</p>
+                </Link>
             </ul>
         </div>
     )
