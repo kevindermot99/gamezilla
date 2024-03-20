4f775dfa5c5695488f72fbe9051e26dca01a9be5
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-20 12:27:45 +0200

    sidebar component UI complete

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 import { MdOutlineExplore } from "react-icons/md";
 import { LuLibrary } from "react-icons/lu";
 import { MdOutlineContactSupport } from "react-icons/md";
-import { MdOutlineSettingsSuggest } from "react-icons/md";
+import { SlSettings } from "react-icons/sl";
 import { SlLike } from "react-icons/sl";
 import { MdOutlineWatchLater } from "react-icons/md";
 import { TbViewportWide } from "react-icons/tb";
@@ -29,7 +29,7 @@ function SideBar() {
 
     return (
         <div className={`transition-width  duration-200 ${sidebar === "big" && 'w-[240px]'}  ${sidebar === "small" && 'w-[70px] py-3 px-2'} relative h-[100vh] flex flex-col items-start justify-start p-3 border-r border-gray-800 border-opacity-50 `}>
-            <div className={`group flex justify-between items-center gap-2 mb-3 px-3 ${sidebar === "small" && 'px-0 justify-center'} w-full`}>
+            <div className={`group flex justify-between items-center gap-2 mb-3 px-3 ${sidebar === "small" && 'px-0 w-[50px]'} w-full`}>
                 
                 <div className={`${sidebar === "small" && 'w-full h-full flex justify-center items-center'} flex justify-start items-center gap-2 text-base font-medium h-[50px] cursor-default`}>
                     <img src={pagesLogo} alt="logo" className=" h-9 w-9" />
@@ -37,9 +37,9 @@ function SideBar() {
                 </div>
                 <button onClick={toggleSidebar} className={`${sidebar === "small" && 'absolute left-[80%] z-30  bg-gray-800 opacity-0 group-hover:opacity-100 '} h-[40px] w-[40px] transition hover:bg-gray-800 flex items-center justify-center rounded-full`}>
                     {sidebar === "big" ? 
-                    <TbViewportNarrow className="text-2xl text-white" />
+                    <TbViewportNarrow className="text-2xl text-white opacity-50" />
                     :
-                    <TbViewportWide className="text-2xl text-white" />
+                    <TbViewportWide className="text-2xl text-white opacity-50" />
                 }
                     
                 </button>
@@ -66,7 +66,7 @@ function SideBar() {
                 </Link>
 
                 <Link className={`h-[50px]  group flex items-center justify-start text-[15px] gap-5 outline-none w-full px-4 transition rounded-lg  hover:bg-gray-800 ${sidebar === "small" && 'px-0 justify-center w-[50px]'}`}>
-                    <MdOutlineSettingsSuggest className='text-[23px] min-h-6 min-w-6 group-hover:text-main-color' />
+                    <SlSettings className='text-[23px] min-h-6 min-w-6 group-hover:text-main-color' />
                     <p className={`font-light overflow-clip whitespace-nowrap ${sidebar === "small" && 'hidden'}`}>Settings</p>
                 </Link>
                 <div className='w-[95%] m-auto h-[1px] bg-gray-800 my-3'></div>
