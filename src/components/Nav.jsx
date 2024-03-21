f02e9d1e26483addec92ef74bba7d30574721e98
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-21 07:39:02 +0200

    whole navbar light + dark

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,7 +18,7 @@ function Nav() {
             
                 <form className='relative w-full max-w-[450px]'>
                     <input className="search transition bg-gray-200 dark:bg-gray-800 bg-opacity-60 rounded-lg text-sm placeholder:text-gray-600 text-black dark:text-primary  " type="search" placeholder="Find a Book.." />
-                    <button type="submit" className="absolute top-0 bottom-0 m-auto right-1 transition hover:bg-slate-300 dark:hover:bg-gray-800 h-[80%] w-[40px] rounded-lg flex justify-center items-center">
+                    <button type="submit" className="absolute top-0 bottom-0 m-auto right-1 transition hover:bg-slate-200 dark:hover:bg-gray-800 h-[80%] w-[40px] rounded-lg flex justify-center items-center">
                         <RiSearch2Line className="text-black dark:text-primary text-lg" />
                     </button>
                 </form>
@@ -30,12 +30,12 @@ function Nav() {
                     </a> */}
 
                     <a href="#" className="group navbar-link p-2 ml-5">
-                        <LuLibrary className="text-2xl cursor-pointer relative transition text-primary group-hover:text-main-color" />
+                        <LuLibrary className="text-2xl cursor-pointer relative transition text-black dark:text-primary group-hover:text-main-color" />
                         <Notification label={9} />
                     </a>
                     <a href="#" className="group navbar-link rounded-full ml-3 flex items-center justify-center relative">
                         <img src={defaultPfp} className=" h-9 w-9 rounded-full " />
-                        <p className=" text-sm font-semibold absolute w-fit whitespace-nowrap bg-gray-900 py-2 px-3 right-0 -bottom-[140%] rounded-m ring-1 ring-gray-800 rounded-lg tranition hidden group-hover:block">Clare thomson</p>
+                        <p className=" text-sm font-semibold absolute w-fit whitespace-nowrap bg-white  dark:bg-gray-900 py-2 px-3 right-0 -bottom-[140%] rounded-m ring-1 ring-gray-200 dark:ring-gray-800 rounded-lg tranition hidden group-hover:block">Clare thomson</p>
                     </a>
 
                 </div>
