dfb7f078dba8c35d7fdd70e3f1448704435fc8fa
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-23 10:33:30 +0200

    limited lines + view all link

--- src/components/BooksBar.jsx
+++ src/components/BooksBar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Books } from '../constants/data'
 import { FaPlay } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import { TbPlayerTrackNext } from "react-icons/tb";
 
 function BooksBar() {
     return (
         <div className="bookbar w-[100%] h-fit flex overflow-x-scroll overscroll-contain pb-3 mb-6">
-            {Books.map((book) => (
-                <div className=' group flex flex-col w-fit h-auto rounded-xl transition duration-100 hover:bg-stone-200 dark:hover:bg-dark-sidebar p-3'>
+            {Books.slice(0, 3).map((book) => (
+                <div key={book.id} className=' group flex flex-col w-fit h-auto rounded-xl transition duration-100 hover:bg-stone-100 dark:hover:bg-dark-sidebar p-3'>
                     <div className='relative h-[180px] w-[180px] aspect-square rounded-lg overflow-clip mb-2 select-none'>
                         <img className="w-full h-full object-cover pointer-events-none" src={book.URL} alt="" />
                         <div className='w-full h-full absolute top-0 left-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition'>
@@ -16,10 +18,17 @@ function BooksBar() {
                         </div>
 
                     </div>
-                    <h1 className=' break-words  whitespace-normal leading-5 my-1 text-base font-normal text-black dark:text-white '>{book.title}</h1>
+                    <h1 className=' break-words  whitespace-normal leading-5 my-1 text-base font-normal text-black dark:text-white max-w-[180px]'>{book.title}</h1>
                     <p className=' text-sm font-medium  text-stone-600 dark:text-stone-500'>{book.author}</p>
                 </div>
             ))}
+
+                <Link className=' group flex flex-col w-fit h-auto rounded-xl transition duration-100 hover:bg-stone-100 dark:hover:bg-dark-sidebar p-3 cursor-pointer'>
+                    <div className='relative h-[100%] w-[180px] overflow-clip select-none flex flex-col items-center justify-center'>
+                        <TbPlayerTrackNext className='text-2xl opacity-70 ' />
+                        <p className='text-base font-extrabold opacity-70'>View All</p>
+                    </div>
+                </Link>
 
         </div>
     )
