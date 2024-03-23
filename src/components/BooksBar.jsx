a38b5fb48bb88f2644bde9ac0a6d17a919ee6294
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-23 10:09:11 +0200

    new section state (continue)

--- src/components/BooksBar.jsx
+++ src/components/BooksBar.jsx
@@ -6,7 +6,7 @@ function BooksBar() {
     return (
         <div className="bookbar w-[100%] h-fit flex overflow-x-scroll overscroll-contain pb-3 mb-6">
             {Books.map((book) => (
-                <div className=' group flex flex-col w-fit h-auto rounded-xl transition duration-100  hover:bg-dark-sidebar p-3'>
+                <div className=' group flex flex-col w-fit h-auto rounded-xl transition duration-100 hover:bg-stone-200 dark:hover:bg-dark-sidebar p-3'>
                     <div className='relative h-[180px] w-[180px] aspect-square rounded-lg overflow-clip mb-2 select-none'>
                         <img className="w-full h-full object-cover pointer-events-none" src={book.URL} alt="" />
                         <div className='w-full h-full absolute top-0 left-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition'>
@@ -16,8 +16,8 @@ function BooksBar() {
                         </div>
 
                     </div>
-                    <h1 className=' break-words  whitespace-normal leading-5 my-1 text-base font-normal text-white  '>{book.title}</h1>
-                    <p className=' text-sm font-medium  text-gray-600 dark:text-stone-500'>{book.author}</p>
+                    <h1 className=' break-words  whitespace-normal leading-5 my-1 text-base font-normal text-black dark:text-white '>{book.title}</h1>
+                    <p className=' text-sm font-medium  text-stone-600 dark:text-stone-500'>{book.author}</p>
                 </div>
             ))}
 
--- src/sections/Browse.jsx
+++ src/sections/Browse.jsx
@@ -4,9 +4,12 @@ import BooksBar from "../components/BooksBar";
 function Browse() {
 
   return (
-    <section className=" flex flex-col gap-2 ml-14 mr-9 overflow-clip relative">
+    <section className=" flex flex-col gap-2 pl-14 mr-9 overflow-clip relative">
       
-      <h1 className="font-bold text-2xl text-black dark:text-primary mb-1">For Kevin Dermot</h1>
+      <h1 className="font-bold text-2xl text-black dark:text-white mb-1">Trending this week</h1>
+      <BooksBar />
+
+      <h1 className="font-bold text-2xl text-black dark:text-white mb-1">Continue Listening</h1>
       <BooksBar />
 
     </section>
