f991d7819c8bd653750679d14e7b46b8bc0914b7
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-23 07:56:57 +0200

    data dubplication for testing overscrolled content

--- src/components/BooksBar.jsx
+++ src/components/BooksBar.jsx
@@ -4,14 +4,14 @@ import bookcover from '../assets/images/bookcover1.jpg'
 
 function BooksBar() {
     return (
-        <div className="bookbar w-[100%] h-fit flex gap-5 overflow-x-auto pb-6">
+        <div className="bookbar w-[100%] h-fit flex gap-5 overflow-x-auto pb-3 mb-6">
             {Books.map((book) => (
                 <div className='flex flex-col w-[180px] h-fit'>
                     <div className='h-[180px] w-[180px] aspect-square rounded-lg overflow-clip mb-2'>
                         <img className="w-full h-full object-cover" src={bookcover} alt="" />
                     </div>
                     <h1 className=' break-words whitespace-normal leading-5 my-1 text-sm '>Harry Potter & the chamber of sectrets</h1>
-                    <p className=' text-sm opacity-50 font-light'>J.K Rowling</p>
+                    <p className=' text-sm text-gray-600 dark:text-gray-400 font-light'>J.K Rowling</p>
                 </div>
             ))}
 
@@ -21,7 +21,7 @@ function BooksBar() {
                         <img className="w-full h-full object-cover" src={bookcover} alt="" />
                     </div>
                     <h1 className=' break-words whitespace-normal leading-5 my-1 text-sm '>Harry Potter & the chamber of sectrets</h1>
-                    <p className=' text-sm opacity-50 font-light'>J.K Rowling</p>
+                    <p className=' text-sm text-gray-600 dark:text-gray-400 font-light'>J.K Rowling</p>
                 </div>
             ))}
 
@@ -31,7 +31,7 @@ function BooksBar() {
                         <img className="w-full h-full object-cover" src={bookcover} alt="" />
                     </div>
                     <h1 className=' break-words whitespace-normal leading-5 my-1 text-sm '>Harry Potter & the chamber of sectrets</h1>
-                    <p className=' text-sm opacity-50 font-light'>J.K Rowling</p>
+                    <p className=' text-sm text-gray-600 dark:text-gray-400 font-light'>J.K Rowling</p>
                 </div>
             ))}
 
@@ -41,7 +41,7 @@ function BooksBar() {
                         <img className="w-full h-full object-cover" src={bookcover} alt="" />
                     </div>
                     <h1 className=' break-words whitespace-normal leading-5 my-1 text-sm '>Harry Potter & the chamber of sectrets</h1>
-                    <p className=' text-sm opacity-50 font-light'>J.K Rowling</p>
+                    <p className=' text-sm text-gray-600 dark:text-gray-400 font-light'>J.K Rowling</p>
                 </div>
             ))}
 
--- src/sections/Browse.jsx
+++ src/sections/Browse.jsx
@@ -5,11 +5,50 @@ function Browse() {
 
   return (
     <section className=" flex flex-col gap-2 ml-14 mr-9 overflow-clip relative">
+      
       <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
       <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
       <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
       <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
       <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
+      <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
+      <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
+      <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
+      <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
+      <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
+      <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
+      <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
+      <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
+      <BooksBar />
+      
+      <h1 className="font-bold text-2xl text-black dark:text-primary mb-4">For Kevin Dermot</h1>
+      <BooksBar />
+
+
     </section>
   );
 }
