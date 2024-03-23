8ed2a75d33762fba7535483502613e93b7cdee76
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-23 10:50:12 +0200

    full inter view motion on pagez app

--- src/sections/Browse.jsx
+++ src/sections/Browse.jsx
@@ -1,15 +1,29 @@
 import React, { useEffect, useRef, useState } from "react";
 import BooksBar from "../components/BooksBar";
+import Reveal from "react-awesome-reveal";
+import { keyframes } from "@emotion/react";
+
+const customAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-500px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
 
 function Browse() {
 
   return (
     <section className=" flex flex-col gap-2 ml-14 mr-9 overflow-clip relative">
       
-      <h1 className="font-bold text-2xl text-black dark:text-white mb-1">Trending this week</h1>
+      <Reveal keyframes={customAnimation} delay={200} triggerOnce><h1 className="font-bold text-2xl text-black dark:text-white mb-1 w-fit">Trending this week</h1></Reveal>
       <BooksBar />
 
-      <h1 className="font-bold text-2xl text-black dark:text-white mb-1">Continue Listening</h1>
+      <Reveal keyframes={customAnimation} delay={200} triggerOnce><h1 className="font-bold text-2xl text-black dark:text-white mb-1">Continue Listening</h1></Reveal>
       <BooksBar />
 
     </section>
