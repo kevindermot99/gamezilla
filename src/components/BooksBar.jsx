512e0332ca2f0b94451d6113c74449ad7a4d0175
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-23 08:28:50 +0200

    book details maped

--- src/components/BooksBar.jsx
+++ src/components/BooksBar.jsx
@@ -6,12 +6,12 @@ function BooksBar() {
     return (
         <div className="bookbar w-[100%] h-fit flex gap-5 overflow-x-auto pb-3 mb-6">
             {Books.map((book) => (
-                <div className='flex flex-col w-[180px] h-fit'>
+                <div className='flex flex-col w-fit h-fit'>
                     <div className='h-[180px] w-[180px] aspect-square rounded-lg overflow-clip mb-2'>
                         <img className="w-full h-full object-cover" src={book.URL} alt="" />
                     </div>
-                    <h1 className=' break-words whitespace-normal leading-5 my-1 text-sm '>Harry Potter & the chamber of sectrets</h1>
-                    <p className=' text-sm text-gray-600 dark:text-gray-400 font-light'>J.K Rowling</p>
+                    <h1 className=' break-words whitespace-normal leading-5 my-1 text-sm '>{book.title}</h1>
+                    <p className=' text-sm text-gray-600 dark:text-stone-400 font-light'>{book.author}</p>
                 </div>
             ))}
 
