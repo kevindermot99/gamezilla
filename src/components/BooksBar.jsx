d26fb2525bd788363770b724de35d4d114e080e5
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-23 08:48:47 +0200

    play icon int

--- src/components/BooksBar.jsx
+++ src/components/BooksBar.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Books } from '../constants/data'
-import bookcover from '../assets/images/bookcover1.jpg'
+import { IoPlayCircleSharp } from "react-icons/io5";
 
 function BooksBar() {
     return (
         <div className="bookbar w-[100%] h-fit flex overflow-x-auto pb-3 mb-6">
             {Books.map((book) => (
-                <div className='flex flex-col w-fit h-auto rounded-xl  hover:bg-dark-sidebar px-3 py-4'>
-                    <div className='h-[180px] w-[180px] aspect-square rounded-lg overflow-clip mb-2'>
+                <div className='flex flex-col w-fit h-auto rounded-xl  hover:bg-dark-sidebar p-3'>
+                    <div className='relative h-[180px] w-[180px] aspect-square rounded-lg overflow-clip mb-2'>
                         <img className="w-full h-full object-cover" src={book.URL} alt="" />
+                        <a className=' absolute bottom-0 right-0'>
+                            <IoPlayCircleSharp className='text-main-color ' />
+                        </a>
                     </div>
                     <h1 className=' break-words whitespace-normal leading-5 my-1 text-sm '>{book.title}</h1>
-                    <p className=' text-sm font-medium  text-gray-600 dark:text-stone-400 font-light'>{book.author}</p>
+                    <p className=' text-sm font-medium  text-gray-600 dark:text-stone-400'>{book.author}</p>
                 </div>
             ))}
 
