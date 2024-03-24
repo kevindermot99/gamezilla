e17405ef9c2f5694b5d0bffc0df4603d1b229d5b
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-24 15:02:17 +0200

    book name as a link

--- src/components/BooksBar.jsx
+++ src/components/BooksBar.jsx
@@ -39,7 +39,7 @@ function BooksBar({ count }) {
                             </div>
 
                         </div>
-                        <Link to={`/book/${book.id}`} className=' break-words  whitespace-normal leading-5 my-1 text-base font-normal text-black dark:text-white max-w-[180px] hover:underline'>{book.title}</Link>
+                        <Link to={`/book/${book.id}`} className=' break-words  whitespace-normal leading-5 my-1 text-base font-normal text-black dark:text-white max-w-[180px] w-fit hover:underline '>{book.title}</Link>
                         <p className=' text-sm font-medium  text-stone-600 dark:text-stone-500'>{book.author}</p>
                     </div>
 
