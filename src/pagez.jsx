96529d129a6eeccbe2e9576272411d4fbd9d87ee
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-24 15:09:23 +0200

    big and small sidebar toggler brought front z-10

--- src/pagez.jsx
+++ src/pagez.jsx
@@ -9,14 +9,14 @@ const Pagez = () => {
     <main className='relative bg-white dark:bg-dark-body text-black dark:text-white flex max-w-[2000px] m-auto'>
 
       
-      <div className='w-fit'>
+      <div className='w-fit z-10'>
         <SideBar />
       </div>
-      <div className='w-full overflow-x-clip '>
+      <div className='w-full overflow-x-clip z-0'>
         <Nav />
-        <section className='xl:padding-1 wide:padding-r w-full'>
+        <div className='xl:padding-1 wide:padding-r w-full'>
           <Browse />
-        </section>
+        </div>
       </div>
 
     </main>
