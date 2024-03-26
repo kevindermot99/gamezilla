5beb6b4e28e0c1945a821c87edd61ef86a1caffa
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-26 07:47:42 +0200

    notification bgopacity

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Notification = ({label}) => {
   
   return (
-    <div className="bg-black dark:bg-white text-white dark:text-black leading-none absolute -right-1 -top-1 aspect-square size-6 rounded-full flex justify-center items-center text-xs font-mono font-bold ">
+    <div className="bg-black dark:bg-white bg-opacity-80 dark:bg-opacity-95 text-white dark:text-black leading-none absolute -right-1 -top-1 aspect-square size-6 rounded-full flex justify-center items-center text-xs font-mono font-bold ">
         {label > 100 ? "99+" : label}
     </div>
   )
