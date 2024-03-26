77bb7b359f6a592e8837d0a1c4daf95b15127974
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-26 06:15:38 +0200

    page error complete

--- src/pages/PageError.jsx
+++ src/pages/PageError.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 function PageError() {
+  const location = useLocation()
+
   return (
-    <div>PageError</div>
+    <div className='p-5 font-mono text-lg leading-relaxed'>
+      <strong className='font-mono '>404 not found,</strong> this page <strong className=' font-mono'>{location.pathname}</strong> is not a Pagez Route. <a href="/" className='text-main-color underline font-bold font-mono'>click here</a> to exit.
+    </div>
   )
 }
 
