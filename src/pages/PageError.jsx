6121a9c4954dbf1578b3ad7b2b77825d14708197
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-26 06:44:29 +0200

    page error dark mode

--- src/pages/PageError.jsx
+++ src/pages/PageError.jsx
@@ -5,7 +5,7 @@ function PageError() {
   const location = useLocation()
 
   return (
-    <div className='p-5 font-mono text-lg leading-relaxed'>
+    <div className='p-5 font-mono text-lg leading-relaxed text-black dark:text-white'>
       <strong className='font-mono '>404 not found,</strong> this page <strong className=' font-mono'>{location.pathname}</strong> is not a Pagez Route. <a href="/" className='text-main-color underline font-bold font-mono'>click here</a> to exit.
     </div>
   )
