f645e449c620ffdf6ab3ffc6e6219863410eba0d
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-03-30 07:13:22 +0200

    sidebar liked books moved outside ul tag

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import pagesLogo from "../assets/images/pagez-logo.png"
 import { GoHome } from "react-icons/go";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { IoSearchOutline } from "react-icons/io5";
 import { BiCategoryAlt } from "react-icons/bi";
 import { FaRegHeart } from "react-icons/fa";
@@ -16,6 +16,7 @@ import { PiMusicNotesPlusDuotone } from "react-icons/pi";
 
 function SideBar() {
     const [sidebar, setSidebar] = useState("big")
+    const location = useLocation()
 
     const toggleSidebar = () => {
         if (sidebar === "big") {
@@ -51,8 +52,9 @@ function SideBar() {
                     <h1 className={`logoText text-stone-400 px-2 py-2  ${sidebar === "small" && 'hidden'}`}>Pagez</h1>
                 </div>
             </div>
-            <ul className={`sidebar w-full h-full overflow-y-auto overflow-x-clip pr-2 overscroll-contain custom-scrollbar `}>
-                <Link to="/" className={`h-[45px]  group flex items-center justify-start text-[15px] gap-2 px-[5px] outline-none w-full transition duration-100 rounded-lg hover:bg-stone-200 dark:hover:bg-dark-navlink  ${sidebar === "small" && 'max-w-[100%] justify-center'}`}>
+            <ul className={`sidebar w-full h-fit pr-2 overscroll-contain `}>
+                <Link to="/" className={`h-[45px]  group flex items-center justify-start text-[15px] gap-2 px-[5px] outline-none w-full transition duration-100 rounded-lg hover:bg-stone-200 dark:hover:bg-dark-navlink  ${sidebar === "small" && 'max-w-[100%] justify-center'} ${location.pathname === '/' && 'bg-stone-200 dark:bg-dark-navlink relative'}`}>
+                    <div className='indicator'></div>
                     <GoHome className='text-[22px] mx-3 min-h-7 min-w-6 opacity-30  group-hover:opacity-100' />
                     <p className={` text-sm font-medium dark:text-stone-300 text-stone-700  overflow-clip whitespace-nowrap ${sidebar === "small" && 'hidden'}`}>Home</p>
                 </Link>
@@ -82,25 +84,25 @@ function SideBar() {
                     <p className={` text-sm font-medium dark:text-stone-300 text-stone-700  overflow-clip whitespace-nowrap ${sidebar === "small" && 'hidden'}`}>History</p>
                 </Link>
 
-
-                <div className='w-[95%] m-auto h-[2px] bg-stone-200 dark:bg-stone-500 dark:bg-opacity-15 my-3'></div>
-
-                <div className="w-full h-fit flex flex-col">
-                    <div className='flex justify-between items-center pr-2 mb-2'>
-                        <p className='text-stone-400 dark:text-stone-300 capitalize text-sm px-2 py-2'>Liked Books</p>
-                        <Link to={`/liked/${id}`} className='text-sm capitalize text-stone-700 dark:text-stone-300 hover:underline'>view all</Link>
-                    </div>
-                    {Books.map((book, index) => (
-                        <Link to={`/book/${book.id}`} key={index} className=' w-full cursor-pointer h-fit flex items-center p-2 gap-3 rounded-lg hover:bg-stone-200 dark:hover:bg-dark-navlink'>
-                            <img src={book.URL} alt={book.title} className='h-10 w-10 shadow-md rounded-sm ' />
-                            <div className={` block w-full max-w-[70%] truncate text-ellipsis ${sidebar === "small" && " hidden"}`}>
-                                <h1 className='truncate text-ellipsistext-black dark:text-white'>{book.title}</h1>
-                                <p className={`truncate text-ellipsis text-stone-500 dark:text-stone-400 text-sm `}>{book.author}</p>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
             </ul>
+
+            <div className='w-[95%] m-auto h-[2px] bg-stone-200 dark:bg-stone-500 dark:bg-opacity-15 my-3'></div>
+
+            <div className="w-full flex flex-col overflow-y-auto custom-scrollbar overflow-x-clip h-full pr-2">
+                <div className='flex justify-between items-center pr-2 mb-2'>
+                    <p className='text-stone-400 dark:text-stone-300 capitalize text-sm px-2 py-2'>Liked Books</p>
+                    <Link to={`/liked/${id}`} className='text-sm capitalize text-stone-700 dark:text-stone-300 hover:underline'>view all</Link>
+                </div>
+                {Books.map((book, index) => (
+                    <Link to={`/book/${book.id}`} key={index} className=' w-full cursor-pointer h-fit flex items-center p-2 gap-3 rounded-lg hover:bg-stone-200 dark:hover:bg-dark-navlink'>
+                        <img src={book.URL} alt={book.title} className='h-10 w-10 shadow-md rounded-sm ' />
+                        <div className={` block w-full max-w-[70%] truncate text-ellipsis ${sidebar === "small" && " hidden"}`}>
+                            <h1 className='truncate text-ellipsistext-black dark:text-white'>{book.title}</h1>
+                            <p className={`truncate text-ellipsis text-stone-500 dark:text-stone-400 text-sm `}>{book.author}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
 
             <div className='absolute bottom-0 left-0 w-full h-fit p-3 bg-white dark:bg-dark-sidebar hidden'>
                 <button onClick={toggleSidebar} className={`group w-full py-3 px-2 flex items-center justify-center rounded-lg hover:bg-main-color transition duration-300`}>
