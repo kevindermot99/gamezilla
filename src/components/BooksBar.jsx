0fbfa36e375317ddc2004c49eb79cdad933de85b
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-04-01 08:59:10 +0200

    pagez browse intro animation

--- src/components/BooksBar.jsx
+++ src/components/BooksBar.jsx
@@ -24,34 +24,30 @@ function BooksBar({ count }) {
 
     return (
         <div className="bookbar w-[100%] h-fit flex overflow-x-scroll overscroll-y-auto overflow-y-clip  overscroll-contain pb-3 mb-6 custom-scrollbar">
-            <Reveal keyframes={customAnimation} triggerOnce duration={300}>
-                {Books.slice(0, limit).map((book) => (
 
-                    <div key={book.id} className=' group flex flex-col w-fit h-[100%] rounded-xl transition duration-100 hover:bg-stone-100 dark:hover:bg-dark-hover p-3'>
-                        <div className='relative h-[180px] w-[180px] aspect-square rounded-md  overflow-clip mb-2 select-none'>
-                            <img className="w-full h-full object-cover pointer-events-none" src={book.URL} alt="" />
-                            <div className='w-full h-full absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition'>
-                                <button className=' transition absolute bottom-2 right-2 aspect-square size-14  flex items-center justify-center pl-[2px]  bg-main-color rounded-full duration-100 translate-y-3 group-hover:translate-y-0 hover:scale-105 shadow-lg' title={`Play ${book.title}`}>
-                                    <FaPlay className='text-white text-xl ' />
-                                </button>
-                            </div>
+            {Books.slice(0, limit).map((book) => (
 
+                <div key={book.id} className=' group flex flex-col w-fit h-[100%] rounded-xl transition duration-100 hover:bg-stone-100 dark:hover:bg-dark-hover p-3'>
+                    <div className='relative h-[180px] w-[180px] aspect-square rounded-md  overflow-clip mb-2 select-none'>
+                        <img className="w-full h-full object-cover pointer-events-none" src={book.URL} alt="" />
+                        <div className='w-full h-full absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition'>
+                            <button className=' transition absolute bottom-2 right-2 aspect-square size-14  flex items-center justify-center pl-[2px]  bg-main-color rounded-full duration-100 translate-y-3 group-hover:translate-y-0 hover:scale-105 shadow-lg' title={`Play ${book.title}`}>
+                                <FaPlay className='text-white text-xl ' />
+                            </button>
                         </div>
-                        <Link to={`/book/${book.id}`} className=' break-words  whitespace-normal leading-5 my-1 text-base font-normal text-black dark:text-white max-w-[180px] w-fit hover:underline '>{book.title}</Link>
-                        <Link to={`/author/${book.author}`} className=' text-sm font-medium  text-stone-600 dark:text-stone-500 hover:underline'>{book.author}</Link>
-                    </div>
 
-                ))}
-            </Reveal>
-            <Reveal keyframes={customAnimation} triggerOnce >
-                <Link className=' group flex flex-col w-fit h-[100%] rounded-xl transition duration-100 hover:bg-stone-100 dark:hover:bg-dark-hover p-3 cursor-pointer'>
-                    <div className='relative h-[100%] w-[180px] overflow-clip select-none flex flex-col items-center justify-center'>
-                        <TbPlayerTrackNext className='text-2xl opacity-70 ' />
-                        <p className='text-base font-medium  opacity-70'>View All</p>
                     </div>
-                </Link>
-            </Reveal>
+                    <Link to={`/book/${book.id}`} className=' break-words  whitespace-normal leading-5 my-1 text-base font-normal text-black dark:text-white max-w-[180px] w-fit hover:underline '>{book.title}</Link>
+                    <Link to={`/author/${book.author}`} className=' text-sm font-medium  text-stone-600 dark:text-stone-500 hover:underline'>{book.author}</Link>
+                </div>
 
+            ))}
+            <Link className=' group flex flex-col w-fit h-[100%] rounded-xl transition duration-100 hover:bg-stone-100 dark:hover:bg-dark-hover p-3 cursor-pointer'>
+                <div className='relative h-[100%] w-[180px] overflow-clip select-none flex flex-col items-center justify-center'>
+                    <TbPlayerTrackNext className='text-2xl opacity-70 ' />
+                    <p className='text-base font-medium  opacity-70'>View All</p>
+                </div>
+            </Link>
         </div>
     )
 }
--- src/sections/Browse.jsx
+++ src/sections/Browse.jsx
@@ -4,15 +4,15 @@ import Reveal from "react-awesome-reveal";
 import { keyframes } from "@emotion/react";
 
 const customAnimation = keyframes`
-  // from {
-  //   opacity: 0;
-  //   transform: translateX(-200px);
-  // }
+  from {
+    opacity: 0;
+    transform: translateY(40px);
+  }
 
-  // to {
-  //   opacity: 1;
-  //   transform: translateX(0);
-  // }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 `;
 
 function Browse() {
@@ -20,27 +20,27 @@ function Browse() {
   return (
     <section className=" flex flex-col gap-2 ml-12 mr-9 overflow-clip relative max-md:ml-3 max-md:mr-3">
 
-      <Reveal keyframes={customAnimation} delay={200} triggerOnce duration={800}>
+      <Reveal keyframes={customAnimation} triggerOnce duration={400}>
         <h1 className="font-bold text-2xl text-black dark:text-white mb-1 px-3 capitalize">
-        Trending Now
+          Trending Now
         </h1>
+        <BooksBar count={9} />
       </Reveal>
-      <BooksBar count={9} />
 
-      <Reveal keyframes={customAnimation} delay={200} triggerOnce duration={800}>
+      <Reveal keyframes={customAnimation} triggerOnce duration={400}>
         <h1 className="font-bold text-2xl text-black dark:text-white mb-1 px-3 capitalize">
-        Recently added
-      </h1>
+          Recently added
+        </h1>
+        <BooksBar count={5} />
       </Reveal>
-      <BooksBar count={5} />
 
-      <Reveal keyframes={customAnimation} delay={200} triggerOnce duration={800}>
+      <Reveal keyframes={customAnimation} triggerOnce duration={400}>
         <h1 className="font-bold text-2xl text-black dark:text-white mb-1 px-3 capitalize">
-        History
-      </h1>
+          History
+        </h1>
+        <BooksBar count={6} />
       </Reveal>
-      <BooksBar count={6} />
-      
+
     </section>
   );
 }
