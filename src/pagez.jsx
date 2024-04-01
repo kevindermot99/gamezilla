c72068b2c8b8e92514db43da31bc9979926c1b1c
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-04-01 09:42:22 +0200

    pagez bg added

--- src/pagez.jsx
+++ src/pagez.jsx
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import SideBar from './components/SideBar';
 import LogoDarkMode from './assets/images/Pagez_Logo__dark_mode.png'
 import LogoLightMode from './assets/images/Pagez_Logo__light_mode.png'
+import PagezDarkBg from './assets/images/pagez-bg.jpg'
 import { GrClose } from "react-icons/gr";
 
 
@@ -45,15 +46,15 @@ const Pagez = () => {
     //   <img src={LogoDarkMode} className='h-20 hidden dark:block' />
     //   <img src={LogoLightMode} className='h-20 block dark:hidden' />
     // </div>
-    <main className='relative bg-white dark:bg-dark-body text-black dark:text-white flex max-w-[2000px] m-auto h-[100vh]'>
-      
+    <main className='home relative bg-white dark:bg-dark-body text-black dark:text-white flex max-w-[2000px] m-auto h-[100vh] overflow-clip'>
+      <img src={PagezDarkBg} className='absolute opacity-5 h-full w-full object-cover hidden dark:block blur-sm  '/>
       <div className={`w-fit z-50 max-md:fixed max-md:top-0 max-md:transition max-md:duration-500  ${sidebar ? 'max-md:opacity-100' : 'max-md:opacity-100 max-md:-translate-x-[100vw]'} `}>
         <SideBar closeSidebar={closeSidebar} />
       </div>
 
       <div className='w-full overflow-x-clip z-0 overflow-y-auto custom-scrollbar'>
         <Nav openSidebar={openSidebar} />
-        <div className='xl:padding-1 wide:padding-r w-full'>
+        <div className='xl:padding-1 wide:padding-r w-full max-md:mt-5'>
           <div className='px-[60px] mb-5 w-fit max-md:px-6'>
             <p className='text-stone-400 dark:text-stone-500  text-sm tracking-wide font-mono cursor-default'>
               {greet} Kevin.
