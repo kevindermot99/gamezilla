1e611b62e157e65e674e5b465224582607b06bd2
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-04-05 07:26:53 +0200

    max width recently added removed

--- src/pages/RecentlyAdded.jsx
+++ src/pages/RecentlyAdded.jsx
@@ -35,7 +35,7 @@ function RecentlyAdded() {
     }
 
     return (
-        <main className={`relative bg-white dark:bg-dark-body max-w-[2000px] m-auto overflow-hidden h-[100vh] `}>
+        <main className={`relative bg-white dark:bg-dark-body overflow-hidden h-[100vh] `}>
 
             {/* <div style={{ backgroundColor: color }} className='h-[500px] max-sm:h-[100vh] w-[100%] absolute top-0 left-0 opacity-100 z-10 transition duration-200'></div> */}
 
