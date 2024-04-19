cc9c5d558c1557e222f852a40d837e79ac274836
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-04-19 07:19:01 +0200

    menu drop down init

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,16 +10,24 @@ function Menu() {
 
   return (
     <div className='h-[55px] px-[24px] border-b border-light-border-line flex items-center'>
-        <div className='h-full w-fit'>
-          <ul className='flex h-[100%] items-center'>
-            <Link to="/" className='text-xl font-bold flex items-center tracking-tighter mr-6'><img className='h-[35px]' src={Logo} alt="" />Pagez</Link>
-            <Link to="/" className={`text-[15px] font-semibold flex items-center tracking-wide mr-5 h-[100%] ${location.pathname === '/' ? 'border-b-[3px]  border-b-light-text-color ' : ''}`}>Explore <GoChevronDown className='ml-[2px]' /></Link>
-            <Link to="/" className='text-[15px] font-semibold flex items-center tracking-wide mr-5'>Library</Link>
-            <div className='w-6 h-[25px] border-l border-l-light-border-line'></div>
-            <Link to="/" className='text-[15px] font-semibold flex items-center tracking-wide mr-5'>Repo</Link>
-            <Link to="/" className='text-[15px] font-semibold flex items-center tracking-wide mr-5'>Rate This App</Link>
-          </ul>
-        </div>
+      <div className='h-full w-fit'>
+        <ul className='flex h-[100%] items-center relative'>
+          <Link to="/" className='text-xl font-bold flex items-center tracking-tighter mr-6'><img className='h-[35px]' src={Logo} alt="" />Pagez</Link>
+          <div className='h-full group mr-5'>
+            <Link to="/" className={`text-[15px] font-semibold flex items-center tracking-wide h-[100%] ${location.pathname === '/' ? 'border-b-[3px]  border-b-light-text-color ' : ''}`}>Explore <GoChevronDown className='ml-[2px]' /></Link>
+            <div className={`absolute top-[90%] left-[70px] min-w-[180px] min-h-[100px] bg-white border border-light-border-line rounded-lg shadow px-6 py-4 flex-col hidden group-hover:flex `}>
+              <Link to={`/`} className={`text-[14px] font-semibold flex items-center tracking-wide pb-2 hover:text-main-color transition duration-100 ${location.pathname === '/' && 'text-main-color '}`}>For You</Link>
+              <Link to={`/`} className={`text-[14px] font-semibold flex items-center tracking-wide pb-2 hover:text-main-color transition duration-100 `}>Search & Explore</Link>
+              <Link to={`/`} className={`text-[14px] font-semibold flex items-center tracking-wide pb-2 hover:text-main-color transition duration-100 `}>Pagez for Kids</Link>
+            </div>
+          </div>
+
+          <Link to="/" className='text-[15px] font-semibold flex items-center tracking-wide mr-5'>Library</Link>
+          <div className='w-6 h-[25px] border-l border-l-light-border-line'></div>
+          <Link to="/" className='text-[15px] font-semibold flex items-center tracking-wide mr-5'>Repo</Link>
+          <Link to="/" className='text-[15px] font-semibold flex items-center tracking-wide mr-5'>Rate This App</Link>
+        </ul>
+      </div>
     </div>
   )
 }
