c51662021395242fc3eb1f95da1fc25491d3bce6
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-04-19 09:06:59 +0200

    hero structure init

--- src/pages/Pagez.jsx
+++ src/pages/Pagez.jsx
@@ -5,8 +5,13 @@ function Pagez() {
   return (
     <div className=' h-svh overflow-y-auto flex flex-col '>
       <Menu />
-      <section>
-        
+      {/* HERO */}
+      <section className='overflow-clip min-h-[600px] min-w-[1500px] mx-auto bg-slate-200 flex items-center justify-between '>
+        <div></div>
+        <div>
+          <h1 className='text-center font-semibold  text-[90px] leading-[83px] capitalize '>The largest <br /> Free Book Library <br /> there is. </h1>
+        </div>
+        <div></div>
       </section>
     </div>
   )
