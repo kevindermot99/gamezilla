3cbfacb619240f204b81e3dd91b843b912902540
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-04-20 15:25:13 +0200

    fixed player init

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { useEffect, useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Pagez from './pages/Pagez';
 import PageError from './pages/PageError';
+import Player from './components/Player';
 
 
 function App() {
 
 
   return (
-    <>
+    <div className='flex flex-col h-svh'>
       <Router>
         <Routes>
           <Route path='/' element={<Pagez />} />
           <Route path='/*' element={<PageError />} />
         </Routes>
       </Router>
-    </>
+
+      <Player />
+    </div>
   );
 }
 
---
+++ src/components/Player.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Player() {
+  return (
+    <div className='z-10 bg-stone-100 w-full h-[55px]'>Player</div>
+  )
+}
+
+export default Player
