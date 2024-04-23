c61c7d904781b6607696fd211a14bd402ab5c24f
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-04-23 11:25:01 +0200

    menu complete

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation } from 'react-router-dom'
 import { GoChevronDown } from "react-icons/go";
 import { TbSearch } from "react-icons/tb";
 import { GoGitBranch } from "react-icons/go";
+import Logo from '../assets/logo.png'
 
 function Menu() {
   const location = useLocation();
@@ -38,7 +39,7 @@ function Menu() {
   }
 
   useEffect(() => {
-    document.title = "Explore Books"
+    document.title = "Explore | GameGrid"
   }, [])
 
   const searchThis = (e) => {
@@ -49,7 +50,7 @@ function Menu() {
     <div className='sticky top-0 z-50 bg-body-color  h-[55px] min-h-[55px] px-[24px] border-b text-light-text-color border-border-line-color flex items-center justify-between'>
       <div className='h-full w-fit min-w-fit'>
         <ul className='flex h-[100%] items-center relative  '>
-          <Link to="/" className='text-xl font-bold flex items-center tracking-tighter mr-6'><img className='h-[35px] mr-1' src='{Logo}' alt="" /> Pagez</Link>
+          <Link to="/" className='text-xl font-bold flex items-center tracking-tighter mr-8'><img src={Logo} className='h-[40px] mr-2' alt="" /> GameGrid</Link>
           <div className='h-full group mr-5'>
             <Link to="/" className={`text-[15px] font-semibold flex items-center tracking-wide h-[100%] transition duration-100 border-[3px] border-transparent hover:border-b-main-color  ${location.pathname === '/' ? ' border-b-main-color ' : ''}`}>Explore <GoChevronDown className='ml-[2px]' /></Link>
             <div className={`absolute top-[90%] left-[70px] min-w-[180px] min-h-[100px] z-10 bg-body-color py-5 border border-border-line-color rounded-lg shadow  flex-col hidden group-hover:flex `}>
@@ -67,7 +68,7 @@ function Menu() {
         </ul>
       </div>
 
-      <form onSubmit={searchThis} className='h-full w-full relative flex text-light-text-color'>
+      <form onSubmit={searchThis} className='h-full w-full max-w-[600px] relative flex text-light-text-color'>
         <label className='w-full relative overflow-hidden flex flex-col items-center justify-center z-20'>
           <input type="search" onChange={handleSearchValue} onKeyDown={hideWhenEmpty} value={searchValue} placeholder='Search' className='bg-container-color placeholder:text-text-color-light  rounded-full h-[65%] w-full pl-9 pr-20 border border-border-line-color text-[15px] tracking-tight font-medium ' />
           <TbSearch className='text-lg absolute top-0 bottom-0 my-auto left-[12px] text-light-text-color opacity-40 ' />
@@ -78,7 +79,7 @@ function Menu() {
           <div className='px-4 flex flex-col mt-3'>
             <Link to={`/`} className='group px-5 py-3 rounded-xl transition hover:bg-container-color '>
               <h1 className=' text-[15px] tracking-tight font-medium transition group-hover:text-main-color  '>Home Lander</h1>
-              <p className='text-sm mt-1 truncate whitespace-break-spaces text-text-color-light line-clamp-2 w-full '>24GB</p>
+              <p className='text-sm mt-1 truncate whitespace-break-spaces text-text-color-light line-clamp-2 w-full '>24GB • Torrent</p>
             </Link>
           </div>
           <span className='w-full h-fit border-b border-border-line-color my-4'></span>
@@ -91,8 +92,8 @@ function Menu() {
 
       <div className={`h-full w-fit min-w-fit relative flex items-center ml-5 space-x-5`}>
         <div className='flex items-center gap-3 h-full w-fit min-w-fit'>
-          <Link to={`/`} className={`h-[65%] w-fit flex items-center px-5 text-main-color hover:bg-main-color/10 border border-border-line-color text-[14px] font-semibold tracking-tight rounded-full `}>Log In</Link>
-          <Link to={`/`} className={`h-[65%] w-fit flex items-center px-5 bg-main-color text-white border-border-line-color text-[14px] font-semibold tracking-tight rounded-full `}>Sign Up</Link>
+          <Link to={`/`} className={`h-[65%] w-fit flex items-center px-5 text-main-color bg-main-color/5 text-[14px] font-semibold tracking-tight rounded-full `}>Log In</Link>
+          <Link to={`/`} className={`h-[65%] w-fit flex items-center px-5 bg-main-color text-white text-[14px] font-semibold tracking-tight rounded-full `}>Sign Up</Link>
         </div>
         <div className='w-fit h-[25px] border-l border-l-border-line-color'></div>
         <Link to="/" className='text-[15px] font-semibold flex items-center tracking-wide h-[100%] transition duration-100 mr-5 border-[3px] border-transparent hover:border-b-main-color  '>SC <GoGitBranch className='ml-[5px]' /></Link>
