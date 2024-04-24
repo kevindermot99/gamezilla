3d963b4d444bcedbb0c8405e5e911bc2143e4136
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-04-24 22:47:03 +0200

    banner fixed

--- src/constants/data.js
+++ src/constants/data.js
@@ -7,15 +7,8 @@ export const AppVersion = {
 
 export const Games = [
   {
-    id: '0F3H03083',
-    banner: 'https://image.api.playstation.com/cdn/UP0006/CUSA00110_00/0ihyT1thfpkaypwhvFZhV1frZtmz13KA.jpg0',
-    screenshots: {
-      screenshot1: 'https://media.moddb.com/cache/images/members/1/422/421372/thumb_620x2000/bf4_2013-11-24_12-16-14-09.jpg',
-      screenshot2: 'https://www.ps3blog.net/wp-content/uploads/Alley.jpg',
-      screenshot3: 'https://www.newgamenetwork.com/images/uploads/gallery/BF4/Battlefield4PC24.jpg',
-      screenshot4: 'https://www.newgamenetwork.com/images/uploads/gallery/Battlefield3/bf3%202011-11-05%2016-39-32-61.jpg',
-      screenshot5: 'https://www.newgamenetwork.com/images/uploads/gallery/Battlefield3/battlefield3_19.jpg',
-    },
+    id: '1',
+    banner: 'https://wallpapers.com/images/hd/1920x1080-battlefield-4-background-alo4o8v9slydkx3u.jpg',
     title: 'Battlefield 4',
     description: 'a 2013 first-person shooter video game developed by DICE and published by Electronic Arts.',
     genres: {
@@ -24,16 +17,9 @@ export const Games = [
     }
   },
   {
-    id: 'HF384J038',
-    banner: 'https://image.api.playstation.com/vulcan/img/rnd/202010/0923/hNKimMvniIuUbhlhPqXey86t.jpg',
-    screenshots: {
-      screenshot5: 'https://images.gamewatcherstatic.com/screenshot/image/6/84/145736/04004119.jpg',
-      screenshot1: 'https://neverendingrealm.com/wp-content/uploads/2021/02/Far-Cry-3-Xbox-360.jpg',
-      screenshot2: 'https://i.imgur.com/a5NmX.jpg',
-      screenshot3: 'https://images.gamewatcherstatic.com/screenshot/image/5/af/145715/04004118.jpg',
-      screenshot4: 'https://images.gamewatcherstatic.com/screenshot/image/6/84/145736/04004119.jpg',
-    },
-    title: 'Far Cry® 3 Classic Edition',
+    id: '2',
+    banner: 'https://getwallpapers.com/wallpaper/full/8/b/3/992283-far-cry-3-wallpapers-1920x1080-cell-phone.jpg',
+    title: 'Far Cry 3 ',
     description: 'Create your own adventure in a single-player, open world island of danger and discovery as you journey through the dark side of humanity.',
     genres: {
       genre1: 'Action',
@@ -41,23 +27,23 @@ export const Games = [
       genre3: 'Adventure',
     }
   },
-  {
-    id: '',
-    banner: '',
-    screenshots: {
-      screenshot5: '',
-      screenshot1: '',
-      screenshot2: '',
-      screenshot3: '',
-      screenshot4: '',
-    },
-    title: '',
-    description: '',
-    genres: {
-      genre1: '',
-      genre2: '',
-    }
-  },
+  // {
+  //   id: '',
+  //   banner: '',
+  //   screenshots: {
+  //     screenshot5: '',
+  //     screenshot1: '',
+  //     screenshot2: '',
+  //     screenshot3: '',
+  //     screenshot4: '',
+  //   },
+  //   title: '',
+  //   description: '',
+  //   genres: {
+  //     genre1: '',
+  //     genre2: '',
+  //   }
+  // },
 ]
 
 export const users = [
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Menu from '../components/Menu'
 import { Link } from 'react-router-dom'
 import bannerImg from '../assets/banner.jpg'
@@ -9,63 +9,39 @@ import { Games } from '../constants/data';
 
 function Home() {
 
+  const id = '1'
+  const bannerGame = Games.find(game => game.id === id)
+  console.log(bannerGame)
 
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
-
-  const randomGames = shuffleArray(Games).slice(0, 1);
-
-  console.log(randomGames)
-
-  
-
-  
   return (
     <div className=' h-svh overflow-y-auto flex flex-col text-text-color'>
       {/* MENU */}
       <Menu />
-      <div className='relative w-full h-fit min-h-[630px] '>
-        <img src={'https://image.api.playstation.com/cdn/UP0006/CUSA00110_00/0ihyT1thfpkaypwhvFZhV1frZtmz13KA.jpg'} className='absolute top-0 left-0 -z-10 w-full h-full object-cover object-top    ' />
-        <div className='absolute top-0 w-full h-full left-0 bg-gradient-to-t from-body-color to-body-color/30 '></div>
-        <div className='relative z-10 w-full h-full flex flex-col items-start justify-end gap-3 px-12 py-3 '>
-          <h1 className='font-extrabold text-[90px] leading-[80px] whitespace-break-spaces break-words truncate max-w-full max-md:text-[35px] max-md:leading-[33px] line-clamp-2 '>
-            BattleField 4
-          </h1>
-          <p className='font-light text-base max-w-[500px] tracking-wide leading-relaxed '>a 2013 first-person shooter video game developed by DICE and published by Electronic Arts.</p>
-          <div className='flex gap-1 items-start justify-start  '>
-            <span className='bg-stone-200/10 hover:bg-stone-200/20 px-3 py-1 text-white/60 capitalize rounded-full text-xs cursor-pointer font-light backdrop-blur-sm'>action</span>
-            <span className='bg-stone-200/10 hover:bg-stone-200/20 px-3 py-1 text-white/60 capitalize rounded-full text-xs cursor-pointer font-light backdrop-blur-sm'>Shooter</span>
-            <span className='bg-stone-200/10 hover:bg-stone-200/20 px-3 py-1 text-white/60 capitalize rounded-full text-xs cursor-pointer font-light backdrop-blur-sm'>Mission</span>
-            <span className='bg-stone-200/10 hover:bg-stone-200/20 px-3 py-1 text-white/60 capitalize rounded-full text-xs cursor-pointer font-light backdrop-blur-sm'>Advanture</span>
-          </div>
-          <div className='flex items-center gap-2'>
-            <button className='bg-main-color h-[45px] flex items-center px-9 rounded-full text-lg font-bold active:scale-95  transition mt-2 '>Download</button>
-            <button className='bg-stone-200/10 hover:bg-stone-200/20 text-white/60 h-[45px] backdrop-blur-sm flex items-center px-9 rounded-full text-lg font-bold active:scale-95  transition mt-2 '>Open Trailer</button>
-          </div>
-
-          <div className='absolute right-12 mb-20 w-[30%] max-w-[350px] bg-transparent max-lg:hidden overflow-clip '>
-            <h1 className='text-base font-medium text-text-color/80 '>Screenshots</h1>
-            <div className='w-full h-full overflow-x-auto flex items-center justify-start gap-2 py-3'>
-              <img src={'https://media.moddb.com/cache/images/members/1/422/421372/thumb_620x2000/bf4_2013-11-24_12-16-14-09.jpg'} className='h-32 w-fit object-cover rounded-2xl border-4 border-stone-200/10 ' alt="" />
-              <img src={'https://www.ps3blog.net/wp-content/uploads/Alley.jpg'} className='h-32 w-fit object-cover rounded-2xl border-4 border-stone-200/10 ' alt="" />
-              <img src={'https://www.newgamenetwork.com/images/uploads/gallery/BF4/Battlefield4PC24.jpg'} className='h-32 w-fit object-cover rounded-2xl border-4 border-stone-200/10 ' alt="" />
-              <img src={'https://www.newgamenetwork.com/images/uploads/gallery/Battlefield3/bf3%202011-11-05%2016-39-32-61.jpg'} className='h-32 w-fit object-cover rounded-2xl border-4 border-stone-200/10 ' alt="" />
-              <img src={'https://www.newgamenetwork.com/images/uploads/gallery/Battlefield3/battlefield3_19.jpg'} className='h-32 w-fit object-cover rounded-2xl border-4 border-stone-200/10 ' alt="" />
+      {bannerGame &&
+        <div className='relative w-full h-fit min-h-[600px] '>
+          <img src={bannerGame.banner} className='absolute top-0 left-0 -z-10 w-full h-full object-cover object-top ' />
+          <div className='absolute top-0 w-full h-full left-0 bg-gradient-to-tr from-body-color/80 to-body-color/10  '></div>
+          <div className='relative z-10 w-full h-full flex flex-col items-start justify-end gap-3 p-12'>
+            <h1 className='font-extrabold text-[70px] leading-[85px] lg:max-w-[1000px] whitespace-break-spaces break-words truncate max-w-full max-md:text-[35px] max-md:leading-[33px] line-clamp-2 '>
+              {bannerGame.title}
+            </h1>
+            <p className='font-light text-base max-w-[600px] tracking-wide leading-relaxed mb-2 '>
+              {bannerGame.description}
+            </p>
+            <div className='flex gap-1 items-start justify-start mb-2'>
+              {Object.values(bannerGame.genres).map((genre, index) => (
+                <span key={index} className='bg-stone-200/10 hover:bg-stone-200/20 px-3 py-1 text-white/60 capitalize rounded-full text-xs cursor-pointer font-light backdrop-blur-sm'>{genre}</span>
+              ))}
             </div>
-          </div>
+            <div className='flex items-center gap-2'>
+              <button className='bg-main-color h-[45px] flex items-center px-9 rounded-full text-lg font-bold active:scale-95  transition mt-2 '>Download</button>
+              <button className='bg-stone-200/10 hover:bg-stone-200/20 text-white/60 h-[45px] backdrop-blur-sm flex items-center px-9 rounded-full text-lg font-bold active:scale-95  transition mt-2 '>Open Trailer</button>
+            </div>
 
-          <div className='bg-transparent self-center my-4 flex  gap-2'>
-            <button className='bg-stone-400/20 hover:bg-stone-200/30 backdrop-blur-sm flex items-center p-2 rounded-full font-bold active:-rotate-90   transition text-2xl'><PiArrowCounterClockwiseFill /></button>
-            <button className='bg-stone-400/20 hover:bg-stone-200/30 backdrop-blur-sm flex items-center p-2 rounded-full font-bold active:rotate-90   transition text-2xl'><PiArrowClockwiseFill /></button>
+            
           </div>
-
         </div>
-      </div>
+      }
 
     </div>
   )
