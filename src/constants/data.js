7b81f352a895d189cc2402bd30c07fe368c6aeb2
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-04-26 12:54:49 +0200

    new games being updated at the moment

--- src/constants/data.js
+++ src/constants/data.js
@@ -14,7 +14,8 @@ export const Games = [
     genres: {
       genre1: 'Action',
       genre2: 'First-person shooter'
-    }
+    },
+    downloads: '239,924'
   },
   {
     id: '2',
@@ -26,25 +27,242 @@ export const Games = [
       genre1: 'Action',
       genre2: 'First-person shooter',
       genre3: 'Adventure',
-    }
+    },
+    downloads: '239,924'
   },
-  // {
-  //   id: '',
-  //   banner: '',
-  //   screenshots: {
-  //     screenshot5: '',
-  //     screenshot1: '',
-  //     screenshot2: '',
-  //     screenshot3: '',
-  //     screenshot4: '',
-  //   },
-  //   title: '',
-  //   description: '',
-  //   genres: {
-  //     genre1: '',
-  //     genre2: '',
-  //   }
-  // },
+  {
+    id: '3',
+    banner: 'https://cdnb.artstation.com/p/assets/images/images/061/794/417/large/favorisxp-fryxyt9wiaaykfs.jpg',
+    poster: 'https://m.media-amazon.com/images/I/81YMKsZX8ZL._AC_UF894,1000_QL80_.jpg',
+    title: 'Halo Infinite',
+    description: 'The next chapter of the legendary franchise begins with a new, epic sci-fi adventure.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'First-person shooter',
+      genre3: 'Adventure'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '4',
+    banner: 'https://wallpapers.com/images/hd/black-ops-3-1920-x-962-wallpaper-uzogqoxju96s3e62.jpg',
+    poster: 'https://cdn11.bigcommerce.com/s-yzgoj/images/stencil/1280x1280/products/3221289/6207654/TIARP14317__63676.1709693113.jpg',
+    title: 'Call of Duty: Black Ops III',
+    description: 'takes place in 2065, 40 years after the events of Black Ops II, in a world facing upheaval from conflicts, climate change and new technologies',
+    genres: {
+      genre1: 'Action',
+      genre2: 'First-person shooter',
+      genre3: 'Adventure'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '5',
+    banner: 'https://cdn.mos.cms.futurecdn.net/6t8UyQj6T7vRte98cKXzvn-1200-80.jpg',
+    poster: 'https://www.ubisoft.com/en-us/game/far-cry-6',
+    title: 'Far Cry 6',
+    description: 'Fight against tyranny and oppression in a tropical paradise filled with danger and revolution.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'First-person shooter',
+      genre3: 'Adventure'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '6',
+    banner: 'https://image.api.playstation.com/vulcan/ap/rnd/202008/2501/FIdGABRLcUgnXIt3L7tK5TGO.png',
+    poster: 'https://www.playstation.com/en-us/games/god-of-war-ragnarok/',
+    title: 'God of War: Ragnarok',
+    description: 'Join Kratos and Atreus on a journey through Norse mythology in this epic sequel to God of War.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'Adventure'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '7',
+    banner: 'https://www.mobygames.com/images/covers/l/693049-hitman-3-playstation-5-front-cover.jpg',
+    poster: 'https://hitman.com/global/home',
+    title: 'Hitman 3',
+    description: 'Become the ultimate assassin as you travel the world and eliminate high-profile targets.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'Stealth'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '8',
+    banner: 'https://www.somagnews.com/wp-content/uploads/2022/01/Final-Fantasy-XVI-will-be-exclusively-PS5-for-6-months.jpg',
+    poster: 'https://square-enix-games.com/en_GB/games/final-fantasy-xvi',
+    title: 'Final Fantasy XVI',
+    description: 'Embark on a grand adventure in the latest installment of the beloved Final Fantasy series.',
+    genres: {
+      genre1: 'Role-playing',
+      genre2: 'Adventure'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '9',
+    banner: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1355150/header.jpg',
+    poster: 'https://store.steampowered.com/app/1355150/Horizon_Forbidden_West/',
+    title: 'Horizon Forbidden West',
+    description: 'Discover a stunning post-apocalyptic world and uncover the mysteries of the Forbidden West.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'Adventure'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '10',
+    banner: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1145360/header.jpg',
+    poster: 'https://store.steampowered.com/app/1145360/Elden_Ring/',
+    title: 'Elden Ring',
+    description: 'Journey through a dark fantasy world filled with terrifying enemies and epic bosses.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'Adventure'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '11',
+    banner: 'https://images.nvidia.com/geforce-com/international/geforce-now/games/just-cause-4/just-cause-4-gfn-hero-1920x1080.jpg',
+    poster: 'https://store.steampowered.com/app/517630/Just_Cause_4/',
+    title: 'Just Cause 4',
+    description: 'Cause chaos and destruction in an open-world sandbox filled with explosive action.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'Adventure'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '12',
+    banner: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1203220/header.jpg',
+    poster: 'https://store.steampowered.com/app/1203220/Dying_Light_2_Stay_Human/',
+    title: 'Dying Light 2: Stay Human',
+    description: 'Survive in a decaying urban environment filled with infected creatures and warring factions.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'Survival horror'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '13',
+    banner: 'https://static-ca.ebgames.ca/images/products/733214/3max.jpg',
+    poster: 'https://www.ea.com/en-gb/games/apex-legends',
+    title: 'Apex Legends',
+    description: 'Battle it out in fast-paced, squad-based combat in this free-to-play battle royale game.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'First-person shooter'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '14',
+    banner: 'https://static-ca.ebgames.ca/images/products/733214/3max.jpg',
+    poster: 'https://www.ea.com/en-gb/games/apex-legends',
+    title: 'Apex Legends',
+    description: 'Battle it out in fast-paced, squad-based combat in this free-to-play battle royale game.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'First-person shooter',
+    },
+    downloads: "N/A"
+  },
+  {
+    id: '16',
+    banner: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1238860/header.jpg',
+    poster: 'https://store.steampowered.com/app/1238860/Back_4_Blood/',
+    title: 'Back 4 Blood',
+    description: 'Fight against hordes of ravenous zombies in intense co-op gameplay from the creators of Left 4 Dead.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'First-person shooter'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '17',
+    banner: 'https://www.gameinformer.com/sites/default/files/styles/thumbnail/public/2022/02/01/7b1f7b3d/elden%20ring%20banner%20-%20revised.png',
+    poster: 'https://en.bandainamcoent.eu/elden-ring/elden-ring',
+    title: 'Elden Ring',
+    description: 'Unravel the mysteries of the Elden Ring and engage in challenging combat in a vast open world.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'Adventure'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '18',
+    banner: 'https://cdn.cloudflare.steamstatic.com/steam/apps/244850/header.jpg',
+    poster: 'https://store.steampowered.com/app/244850/The_Forest/',
+    title: 'The Forest',
+    description: 'Survive in a mysterious forest filled with cannibalistic mutants in this open-world survival horror game.',
+    genres: {
+      genre1: 'Survival',
+      genre2: 'Horror'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '19',
+    banner: 'https://images.nvidia.com/geforce-com/international/geforce-now/games/horizon-zero-dawn-complete-edition/horizon-zero-dawn-complete-edition-gfn-hero-1920x1080.jpg',
+    poster: 'https://store.steampowered.com/app/1151640/Horizon_Zero_Dawn_Complete_Edition/',
+    title: 'Horizon Zero Dawn',
+    description: 'Uncover the secrets of a post-apocalyptic world overrun by robotic creatures in this action RPG.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'Adventure'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '20',
+    banner: 'https://www.twinfinite.net/wp-content/uploads/2021/07/Nier-Automata.jpg',
+    poster: 'https://store.steampowered.com/app/524220/NieR_Automata/',
+    title: 'NieR: Automata',
+    description: 'Experience a captivating story and fast-paced combat in this action-packed RPG set in a dystopian future.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'Role-playing'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '21',
+    banner: 'https://cdn.cloudflare.steamstatic.com/steam/apps/292030/header.jpg',
+    poster: 'https://store.steampowered.com/app/292030/The_Witcher_3_Wild_Hunt/',
+    title: 'The Witcher 3: Wild Hunt',
+    description: 'Embark on an epic journey as Geralt of Rivia in this critically acclaimed open-world RPG.',
+    genres: {
+      genre1: 'Action',
+      genre2: 'Adventure',
+      genre3: 'Role-playing'
+    },
+    downloads: 'N/A'
+  },
+  {
+    id: '22',
+    banner: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg',
+    poster: 'https://upload.wikimedia.org/wikipedia/en/2/2c/Resident_Evil_Village.png',
+    title: 'Resident Evil Village',
+    description: 'Face off against grotesque creatures and uncover dark secrets in this survival horror game.',
+    genres: {
+      genre1: 'Survival',
+      genre2: 'Horror'
+    },
+    downloads: 'N/A'
+  },
+
 ]
 
 export const users = [
