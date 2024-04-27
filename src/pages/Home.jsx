62a828c755023f32e41b4df8aea8b91299f78e93
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-04-27 18:49:13 +0200

    Login axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,14 +39,14 @@ function Home() {
             <p className='font-light text-base max-w-[650px] tracking-wide leading-relaxed mb-4  line-clamp-3'>
               {bannerGame.description}
             </p>
-            <div className='flex max-sm:flex-wrap gap-1 items-start justify-start mb-4'>
+            <div className='flex max-sm:flex-wrap gap-2 items-start justify-start mb-4'>
               {Object.values(bannerGame.genres).map((genre, index) => (
-                <span key={index} className='bg-stone-200/20 hover:bg-stone-200/50 px-3 py-1 text-white/60 capitalize rounded-full text-xs cursor-pointer font-light backdrop-blur-sm'>{genre}</span>
+                <span key={index} className='bg-stone-200/20 hover:bg-stone-200/50 px-3 py-1 text-white/60  ring-1 ring-stone-300/30 capitalize rounded-full text-xs cursor-pointer font-light backdrop-blur-sm'>{genre}</span>
               ))}
             </div>
             <div className='flex max-sm:flex-wrap items-center gap-2 max-sm:gap-0'>
               <button className='bg-main-color h-[45px] max-sm:w-full flex items-center max-sm:justify-center px-9 rounded-full text-lg font-bold active:scale-95  transition mt-2 '>Download</button>
-              <button className='bg-stone-200/20 hover:bg-stone-200/40 text-white/60 h-[45px] max-sm:w-full backdrop-blur-sm flex items-center max-sm:justify-center px-9 rounded-full text-lg font-bold active:scale-95  transition mt-2 '>First look?</button>
+              <button className='bg-stone-200/20 hover:bg-stone-200/40 text-white/60 h-[45px] ring-1 ring-stone-300/30 max-sm:w-full backdrop-blur-sm flex items-center max-sm:justify-center px-9 rounded-full text-lg font-bold active:scale-95  transition mt-2 '>First look?</button>
               <div className='relative w-fit group h-fit min-h-[45px] max-sm:hidden flex items-end ml-1'>
                 <p className='absolute bottom-[55px] -left-[30px] whitespace-nowrap bg-stone-200/10 backdrop-blur-md py-2 px-4 rounded-full text-sm font-medium tracking-wide  text-text-color/70 pointer-events-none opacity-0 scale-95 transition group-hover:opacity-100 group-hover:scale-100 shadow-lg text-center '>Most Downloaded Badge</p>
                 <FaMedal className='text-stone-200/30 text-[35px] cursor-help transition group-hover:text-stone-200/70 group-hover:scale-105 ' />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import authbg from '../assets/authbg.jpg'
+import axios from 'axios'
 
 function Login() {
+    const [username, setUsername] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [error, setError] = useState(null)
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+
+        try{
+            const response = await axios.post('/login', {
+                username,
+                password
+            })
+
+            if(response.status === 200) {
+                console.log('Login successful')
+            } else {
+                setError('Login Failed')
+            }
+        } 
+        catch (error) {
+            console.log(error)
+            setError('an error occured during login!')
+        }
+
+
+    }
+
   return (
-    <div className='px-[24px] py-4 flex flex-col h-full w-full bg-container-color'>
+    <div className='px-[24px] py-5 flex flex-col h-full w-full bg-body-color '>
         <Link to={'/'} className='text-2xl font-bold flex items-center tracking-tight mr-8 w-fit'>Gamezilla.</Link>
         <div className='h-full w-full flex items-center justify-center flex-col'>
-            <form className='bg-body-color/40 px-9 py-8 rounded-3xl  text-text-color w-full max-w-[420px] flex flex-col items-start justify-start '>
+            <form onSubmit={handleLogin} className=' text-text-color w-full max-w-[360px] flex flex-col items-start justify-start '>
                 <h1 className='text-3xl leading-[35px] font-bold '>Login</h1>
-                <p className='mb-5 text-text-color/80 font-light text-sm'>Hi, Welcome Back</p>
+                <p className='mb-5 text-text-color/20 font-light text-base pt-1'>Hi, Welcome Back</p>
 
                 <label className='w-full flex flex-col mb-2'>
                     <h1 className=' text-text-color/80 font-light mb-2 text-sm'>Email</h1>
@@ -21,7 +48,7 @@ function Login() {
                 </label>
                 <a className='mb-5 text-main-color  font-light flex text-sm self-end'> Forgot Password?</a>
                 <div className='w-full flex gap-2 mb-4 '>
-                    <Link to="/" name='email' className=' w-full py-2 px-4 transition bg-border-line-color hover:bg-stone-400/40 rounded-full text-sm flex items-center justify-center '>Cancel</Link>
+                    <Link to="/" name='email' className=' w-full py-2 px-4 transition bg-border-line-color/60 hover:bg-border-line-color rounded-full text-sm flex items-center justify-center '>Cancel</Link>
                     <button type="submit" name='email' className=' w-full py-2 px-4 transition bg-main-color hover:bg-main-color/60 rounded-full text-sm flex items-center justify-center'>Login</button>
                 </div>
             </form>
