84007ea359c74d1a537f449301999b6c01450714
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-03 20:41:01 +0200

    menu 20%

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -9,6 +9,7 @@ import { TbLogout2 } from "react-icons/tb";
 import { RiAccountCircleLine } from "react-icons/ri";
 import { MdOutlineDownloading } from "react-icons/md";
 import { HiCollection } from "react-icons/hi";
+import { AiOutlineUser } from "react-icons/ai";
 
 function Menu({ userId }) {
   const location = useLocation();
@@ -78,48 +79,49 @@ function Menu({ userId }) {
 
 
   return (
-    <div className={` sticky top-0 z-20 h-fit w-full px-[24px] py-3 text-light-text-color flex items-center justify-between transition-colors ease-in-out duration-500 bg-body-color  ${scrollPosition > 20 ? '  ' : '  '} `}>
-    
-      <form onSubmit={searchThis} className='h-fit w-full max-w-[400px] relative flex text-light-text-color'>
-        <label className='w-full h-fit relative overflow-hidden flex flex-col items-center justify-center z-20'>
-          <input type="search" onChange={handleSearchValue} onKeyDown={hideWhenEmpty} value={searchValue} placeholder='Search' className='bg-container-color placeholder:text-text-color-light rounded-full h-[35px] w-full pl-9 pr-20 text-[15px] tracking-tight font-medium ' />
-          <TbSearch className='text-lg absolute top-0 bottom-0 my-auto left-[12px] text-light-text-color opacity-40 ' />
-          <button onClick={handleResetSearch} className={`absolute top-0 bottom-0 my-auto right-[8px] text-[14px] font-semibold items-center tracking-wide px-3 text-main-color transition  ${resetSearch ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-20'} `}>Cancel</button>
-        </label>
-        <div className={`min-h-fit w-full pt-6 pb-3 bg-body-color shadow-xl absolute top-[10px] border border-border-line-color/40 rounded-2xl flex-col ${quickResult ? 'flex ' : 'hidden'} `}>
-          <div className='px-3 flex flex-col mt-3'>
-            <Link to={`/`} className='group px-4 py-2 rounded-xl transition hover:bg-container-color '>
-              <h1 className=' text-[15px] tracking-wide font-medium transition group-hover:text-main-color  '>Home Lander</h1>
-            </Link>
+    <div className={` sticky top-0 z-20 h-fit w-full px-[24px] py-4 text-light-text-color bg-body-color  ${scrollPosition > 20 ? '  ' : '  '} `}>
+
+      <div className='w-full flex items-center justify-around'>
+        <div>
+          <Link to='/' className='font-bold text-2xl font-SulphurPoint flex items-center justify-center gap-2 '>
+            <img src={Logo} className="h-9" />
+            Gamezilla
+          </Link>
+        </div>
+
+        <form onSubmit={searchThis} className='h-fit w-full max-w-[400px] relative flex text-light-text-color'>
+          <label className='w-full h-fit relative overflow-hidden p-[2px] flex flex-col items-center justify-center z-20'>
+            <input type="search" onChange={handleSearchValue} onKeyDown={hideWhenEmpty} value={searchValue} placeholder='Search' className='bg-body-color ring-1 ring-border-line-color  placeholder:text-text-color-light rounded-full h-[35px] w-full pl-9 pr-20 text-[15px] tracking-tight font-medium ' />
+            <TbSearch className='text-lg absolute top-0 bottom-0 my-auto left-[12px] text-light-text-color opacity-40 ' />
+            <button onClick={handleResetSearch} className={`absolute top-0 bottom-0 my-auto right-[8px] text-[14px] font-semibold items-center tracking-wide px-3 text-main-color transition  ${resetSearch ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-20'} `}>Cancel</button>
+          </label>
+        </form>
+
+        <div className={`h-full w-fit min-w-fit relative flex items-center ml-5 space-x-5`}>
+          <div className={`flex items-center gap-3 h-full w-fit min-w-fit ${userId !== 'none' && 'hidden'}`}>
+            <Link to={`/login`} className={`h-[35px] w-fit flex items-center px-5 text-white text-[15px] font-SulphurPoint font-bold tracking-wide rounded-full transition hover:opacity-70 `}>Log In</Link>
+            <Link to={`/signup`} className={`h-[35px] w-fit flex items-center px-5 bg-main-color text-white text-[15px] font-SulphurPoint font-bold tracking-wide rounded-full `}>Sign Up</Link>
           </div>
-        </div>
-      </form>
-
-      <div className={`h-full w-fit min-w-fit relative flex items-center ml-5 space-x-5`}>
-
-        <div className={`flex items-center gap-3 h-full w-fit min-w-fit ${userId !== 'none' && 'hidden'}`}>
-          <Link to={`/login`} className={`h-[35px] w-fit flex items-center px-5 text-white text-[14px] font-semibold tracking-tight rounded-full transition hover:opacity-70 `}>Log In</Link>
-          <Link to={`/signup`} className={`h-[35px] w-fit flex items-center px-5 bg-main-color text-white text-[14px] font-semibold tracking-tight rounded-full `}>Sign Up</Link>
-        </div>
-        {userId !== 'none' && (
-          <div className={`group flex items-center gap-2 h-full w-fit min-w-fit cursor-default relative ${userId === 'none' && 'hidden'}`}>
-            <p className='bg-main-color  backdrop-blur-md text-white h-8 aspect-square flex items-center justify-center capitalize font-mono font-bold text-2xl rounded-full  '>{username.charAt(0)}</p>
-            <p>{username}</p>
-            <div className='absolute top-[30px] pt-3 right-0 w-fit min-w-[200px] h-fit opacity-0 -z-50 pointer-events-none group-hover:opacity-100 group-hover:z-10 -translate-y-4 group-hover:translate-y-0 transition group-hover:pointer-events-auto   '>
-              <div className='bg-container-color bad rounded-lg p-2 w-full h-full'>
-                <button className='text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-main-color font-normal capitalize '><RiAccountCircleLine className=' text-[25px] cursor-pointer ' /> Account</button>
-                <button className='text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-main-color font-normal capitalize '><MdOutlineDownloading className=' text-[25px] cursor-pointer ' /> Downloads</button>
-                <button className='text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-main-color font-normal capitalize '><HiCollection className=' text-[25px] cursor-pointer ' /> Collection</button>
-                <div className='w-full h-[1px] bg-border-line-color/60 my-2'></div>
-                <button onClick={handleLogout} className='text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-red-500 font-normal capitalize '><TbLogout2 className='text-[25px] cursor-pointer ' /> Logout</button>
+          {userId !== 'none' && (
+            <div className={`group flex items-center gap-2 h-full w-fit min-w-fit cursor-default relative ${userId === 'none' && 'hidden'}`}>
+              <p className='bg-main-color  backdrop-blur-md text-white h-8 aspect-square flex items-center justify-center capitalize font-mono font-bold text-2xl rounded-full  '>{username.charAt(0)}</p>
+              <p>{username}</p>
+              <div className='absolute top-[30px] pt-3 right-0 w-fit min-w-[200px] h-fit opacity-0 -z-50 pointer-events-none group-hover:opacity-100 group-hover:z-10 -translate-y-4 group-hover:translate-y-0 transition group-hover:pointer-events-auto   '>
+                <div className='bg-container-color bad rounded-lg p-2 w-full h-full'>
+                  <button className='text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-main-color font-normal capitalize '><RiAccountCircleLine className=' text-[25px] cursor-pointer ' /> Account</button>
+                  <button className='text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-main-color font-normal capitalize '><MdOutlineDownloading className=' text-[25px] cursor-pointer ' /> Downloads</button>
+                  <button className='text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-main-color font-normal capitalize '><HiCollection className=' text-[25px] cursor-pointer ' /> Collection</button>
+                  <div className='w-full h-[1px] bg-border-line-color/60 my-2'></div>
+                  <button onClick={handleLogout} className='text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-red-500 font-normal capitalize '><TbLogout2 className='text-[25px] cursor-pointer ' /> Logout</button>
+                </div>
               </div>
             </div>
-          </div>
-        )
-        }
+          )
+          }
 
-        <div className='w-fit h-[25px] border-l border-l-stone-300'></div>
-        <a href="https://github.com/kevindermot99/gamezilla" target='_blank' className='text-[15px] font-semibold flex items-center tracking-wide h-fit transition hover:opacity-70 ' title={`Source Code`} >SC <GoGitBranch className='ml-[5px]' /></a>
+          <div className='w-fit h-[25px] border-l border-l-stone-300'></div>
+          <a href="https://github.com/kevindermot99/gamezilla" target='_blank' className='text-[15px] font-semibold flex items-center tracking-wide h-fit transition hover:opacity-70 ' title={`Source Code`} >SC <GoGitBranch className='ml-[5px]' /></a>
+        </div>
       </div>
     </div>
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,72 +24,11 @@ function Home() {
     }
   }, [])
 
-
-  const handleLoading = () => {
-    setIsLoading(true)
-  }
-
-
-  const bannerGameId = "5"
-  const bannerGame = Games.find(game => game.id === bannerGameId)
-
-
   return (
     <div className={`relative h-svh flex text-text-color`} >
 
-      <div className='w-fit min-w-fit'>
-        <Sidebar />
-      </div>
-
-      <div className='w-fit h-full overflow-y-auto pb-10 bg-body-color '>
-
-        {/* MENU */}
         <Menu userId={userId} />
-        <div className='h-fit min-h-[400px] w-full bg-transparent '>
-          
-        </div>
-
-        {/* publishers */}
-        <div className='px-12 pt-5 max-sm:p-5 max-w-[1500px] h-fit mx-auto'>
-          <h1 className='text-base tracking-wide font-semibold p-2'>Publishers</h1>
-          <div className='overflow-x-auto snap-x snap-mandatory grid grid-flow-col-dense mt-5 gap-1 min-h-[250px] h-fit '>
-            {Games.map((game, index) => (
-              <Link key={index} to={`/game/${game.id}`} className='group px-1 snap-start p-1 flex flex-col justify-start items-start h-full w-fit'>
-                <div className='relative p-1 h-[160px] w-[160px] ring-2 ring-transparent group-hover:ring-main-color rounded-full'>
-                  <img src={game.poster} className='pointer-events-none bg-gradient-to-br from-body-color to-border-line-color  aspect-square h-full w-full object-cover object-top rounded-full opacity-85 group-hover:opacity-65  ' />
-                </div>
-                <h1 className='font-normal text-base p-1 truncate max-w-[170px] pt-2'>
-                  {game.title}
-                </h1>
-                <p className='font-bold text-sm text-text-color-light  px-1 '>23,943 Games</p>
-              </Link>
-            ))}
-
-          </div>
-        </div>
-
-        {/* Recommended*/}
-        <div className='px-12 pt-5 max-sm:p-5 mt-4 max-w-[1500px] h-fit mx-auto'>
-          <h1 className='text-base tracking-wide font-semibold p-2'>Recommended</h1>
-          <div className='overflow-x-auto snap-x snap-mandatory p-2 grid grid-flow-col-dense  gap-1 min-h-[250px] h-fit '>
-            {Games.map((game, index) => (
-              <Link key={index} to={`/game/${game.id}`} className='group snap-start p-1 flex flex-col justify-start items-start h-full w-fit'>
-                <div className='relative p-[2px] h-[230px] w-[180px] ring-2 ring-transparent group-hover:ring-main-color rounded-2xl'>
-                  <img src={game.poster} className='pointer-events-none bg-container-color aspect-square h-full rounded-2xl  w-full object-cover object-top opacity-85 group-hover:opacity-65  ' />
-                </div>
-                <h1 className='font-normal text-base p-1 truncate max-w-[170px] pt-2 '>
-                  {game.title}
-                </h1>
-                <p className='font-bold text-sm text-green-600 px-1 '>$ 0.00</p>
-
-              </Link>
-            ))}
-
-          </div>
-        </div>
-
-      </div>
-
+        
     </div>
   )
 }
