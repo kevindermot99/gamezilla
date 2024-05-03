727440dcd1feaa30bfc4cb70dfbaee2cbecbc813
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-03 19:43:31 +0200

    grid update #2

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Sidebar() {
   return (
-    <div className='w-[270px] bg-body-color h-full'>sidebar</div>
+    <div className='w-[270px] bg-body-color h-full flex flex-col'>
+      <h1>Gamezilla</h1>
+      <ul>
+        <Link></Link>
+        <Link></Link>
+      </ul>
+    </div>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -41,18 +41,18 @@ function Home() {
         <Sidebar />
       </div>
 
-      <div className='w-fit h-full overflow-y-auto pb-10 '>
+      <div className='w-fit h-full overflow-y-auto pb-10 bg-body-color '>
 
         {/* MENU */}
         <Menu userId={userId} />
         <div className='h-fit min-h-[400px] w-full bg-transparent '>
-          <img src={bannerGame.banner} alt="" className='fixed top-0 left-0 w-full h-full object-cover -z-10 opacity-5 blur-lg ' />
+          
         </div>
 
         {/* publishers */}
         <div className='px-12 pt-5 max-sm:p-5 max-w-[1500px] h-fit mx-auto'>
-          <h1 className='text-2xl tracking-wide font-bold '><span className='text-text-color-light/40 '>#</span>  Publishers</h1>
-          <div className='overflow-x-auto snap-x snap-mandatory p-2 grid grid-flow-col-dense mt-5 gap-1 min-h-[250px] h-fit '>
+          <h1 className='text-base tracking-wide font-semibold p-2'>Publishers</h1>
+          <div className='overflow-x-auto snap-x snap-mandatory grid grid-flow-col-dense mt-5 gap-1 min-h-[250px] h-fit '>
             {Games.map((game, index) => (
               <Link key={index} to={`/game/${game.id}`} className='group px-1 snap-start p-1 flex flex-col justify-start items-start h-full w-fit'>
                 <div className='relative p-1 h-[160px] w-[160px] ring-2 ring-transparent group-hover:ring-main-color rounded-full'>
@@ -70,12 +70,12 @@ function Home() {
 
         {/* Recommended*/}
         <div className='px-12 pt-5 max-sm:p-5 mt-4 max-w-[1500px] h-fit mx-auto'>
-          <h1 className='text-2xl tracking-wide font-bold '><span className='text-text-color-light/40 '>#</span>  Recommended</h1>
-          <div className='overflow-x-auto snap-x snap-mandatory p-2 grid grid-flow-col-dense mt-5 gap-1 min-h-[250px] h-fit '>
+          <h1 className='text-base tracking-wide font-semibold p-2'>Recommended</h1>
+          <div className='overflow-x-auto snap-x snap-mandatory p-2 grid grid-flow-col-dense  gap-1 min-h-[250px] h-fit '>
             {Games.map((game, index) => (
               <Link key={index} to={`/game/${game.id}`} className='group snap-start p-1 flex flex-col justify-start items-start h-full w-fit'>
-                <div className='relative p-1 h-[230px] w-[180px] ring-2 ring-transparent group-hover:ring-main-color rounded-xl'>
-                  <img src={game.poster} className='pointer-events-none bg-container-color aspect-square h-full rounded-xl w-full object-cover object-top opacity-85 group-hover:opacity-65  ' />
+                <div className='relative p-[2px] h-[230px] w-[180px] ring-2 ring-transparent group-hover:ring-main-color rounded-2xl'>
+                  <img src={game.poster} className='pointer-events-none bg-container-color aspect-square h-full rounded-2xl  w-full object-cover object-top opacity-85 group-hover:opacity-65  ' />
                 </div>
                 <h1 className='font-normal text-base p-1 truncate max-w-[170px] pt-2 '>
                   {game.title}
