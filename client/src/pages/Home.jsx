bf3cec468ea22cebdd3a8a337021ff0cb3ee6891
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-04 08:47:19 +0200

    banner complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,6 +12,8 @@ import { Navigation, Pagination, Autoplay, FreeMode } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
+import { BsCart } from "react-icons/bs";
+
 
 function Home() {
   const [isLoading, setIsLoading] = useState(false)
@@ -32,6 +34,8 @@ function Home() {
     }
   }, [])
 
+  const sortedGames = Games.sort(() => Math.random() - 0.5);
+
   // swiper init
   const swiper = new Swiper('.swiper', {
     // configure Swiper to use modules
@@ -51,7 +55,7 @@ function Home() {
       slideShadows: false,
     },
     // autoplay: {
-    //   delay: 2000,
+    //   delay: 1300,
     //   pauseOnMouseEnter: true
     // }
 
@@ -63,18 +67,23 @@ function Home() {
       <Menu userId={userId} />
 
       <div className='w-full h-fit'>
-        <div className="swiper ">
-          <div className="swiper-wrapper w-full h-fit px-20 py-2">
-            {Games.map((game, index) => (
-              <Link to={`/game/${game.id}`} className="swiper-slide w-full h-full rounded-2xl overflow-clip relative flex flex-col items-start justify-end transition ring-[2px] ring-transparent hover:ring-main-color ">
-                <div className='absolute top-0 left-0 right-0 z-10 scale-110  bg-gradient-to-tr from-body-color to-transparent w-full h-full'></div>
-                <img src={game.banner} className='absolute top-0 left-0 right-0 z-0  bg-gradient-to-br from-container-color to-border-line-color w-full h-full object-cover scale-105 rounded-2xl' />
-                <div className='z-20 p-8'>
-                  <h1 className='font-extrabold text-[35px] tracking-tight leading-tight'>{game.title}</h1>
-                  <p className=' line-clamp-2 max-w-[80%] font-medium text-sm leading-4 capitalize text-text-color/70 '>{game.description}</p>
-
+        <div className="swiper h-fit">
+          <div className="swiper-wrapper w-full h-full px-20 py-4">
+            {sortedGames.map((game, index) => (
+              <div className="swiper-slide w-full h-[330px] rounded-3xl overflow-clip relative flex flex-col items-start justify-end ">
+                <div className='absolute top-0 left-0 right-0 z-10 scale-150 bg-gradient-to-tr from-body-color to-black/10 w-full h-full'></div>
+                <img src={game.banner} className='absolute top-0 left-0 right-0 m-auto z-0  bg-gradient-to-br from-container-color to-border-line-color w-full h-full object-cover object-top scale-105' />
+                <div className='z-20 py-5 px-9'>
+                  <h1 className='font-extrabold text-[30px] tracking-tight leading-tight'>{game.title}</h1>
+                  <p className=' line-clamp-2 max-w-[80%] pt-1 font-medium text-[14px] leading-5 capitalize text-text-color/70 '>{game.description}</p>
+                  <div className='flex gap-2 py-4'>
+                    <Link to={`/game/${game.id}`} className='bg-white/15 hover:bg-white/30 transition text-white backdrop-blur-sm h-9 w-full max-w-[120px] flex items-center justify-center capitalize font-bold text-sm rounded-full'>view</Link>
+                    <button className='bg-white h-9 aspect-square flex justify-center items-center rounded-full '>
+                      <BsCart className='text-black ' />
+                    </button>
+                  </div>
                 </div>
-              </Link>
+              </div>
             ))}
           </div>
         </div>
