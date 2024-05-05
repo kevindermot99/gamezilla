fe26f9d68807283f5b56536de8c3de1a0a883315
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-05 14:28:07 +0200

    font bold

--- client/src/components/ButtonLink.jsx
+++ client/src/components/ButtonLink.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function ButtonLink({ title, to, icon}) {
     return (
         <Link to={to} className="relative cursor-pointer opacity-90 hover:opacity-100 transition-opacity p-[1px] bg-black rounded-xl bg-gradient-to-t from-[#8122b0] to-[#dc98fd] active:scale-95">
-            <div className="w-full h-full flex items-center justify-center gap-1 px-4 whitespace-nowrap py-[10px] bg-main-color text-[#f1d5fe] font-medium rounded-xl bg-gradient-to-t from-[#a62ce2] to-[#8019a0] text-sm">
+            <div className="w-full h-full flex items-center justify-center gap-1 pl-4 pr-5 whitespace-nowrap py-[10px] bg-main-color text-[#f1d5fe] font-semibold rounded-xl bg-gradient-to-t from-[#a62ce2] to-[#8019a0] text-sm">
                 <span className='text-[17px] pb-[1px]  '>{icon}</span>
                 {title}
             </div>
