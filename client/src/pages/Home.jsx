72be19884fc46957e372e7e295b30675095232b5
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-05 10:29:55 +0200

    Hero text font

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -68,14 +68,14 @@ function Home() {
       <div className='w-full h-fit flex flex-col py-8 gap-5 mb-10'>
         
         <div className='w-full h-fit flex items-center justify-center flex-col gap-4 z-20'>
-          <h1 className='text-[60px] leading-[62px] font-montserrat capitalize font-bold text-center'>Open-Source Game Store <br />for Gamers</h1>
-          <p className='font-DMsans font-semibold text-text-color-light text-[17px] text-center max-w-[400px]'> Browse, Download, List, Request a game,... all for absolutely Free of charge.</p>
+          <h1 className='text-[60px] leading-[62px] font-montserrat capitalize font-bold tracking-tight text-center'>Open-Source Game Store <br />for Gamers</h1>
+          <p className='font-montserrat font-semibold text-text-color-light text-[17px] text-center max-w-[400px]'> Browse, Download, List, Request a game,... all for absolutely Free of charge.</p>
         </div>
         
         <div className='w-full translate-y-12 z-10 px-4'>
           <form className='relative w-full flex items-center justify-center'>
             <label className='w-full h-[55px] max-w-[500px] relative'>
-            <input type="search" name="search" autoComplete='off' placeholder='Search for Games' className=' placeholder:text-text-color-light bg-text-color py-0 pl-7 pr-28 text-black text-lg tracking-tight font-medium rounded-2xl w-full h-[55px] ring-8 ring-transparent transition duration-500 focus:ring-4 focus:ring-main-color' />
+            <input type="search" name="search" autoComplete='off' placeholder='Search for Games' className=' placeholder:text-text-color-light bg-text-color py-0 pl-7 pr-28 text-black text-[16px] tracking-tight font-medium rounded-2xl w-full h-[55px] ring-8 ring-transparent transition duration-500 focus:ring-4 focus:ring-main-color' />
             <span className='absolute top-0 bottom-0 right-2 m-auto flex h-fit  '>
               <ButtonClick title={'Search'} type={'submit'} />
             </span>
