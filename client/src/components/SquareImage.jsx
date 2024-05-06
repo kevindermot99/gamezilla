e8f41271c8c9e373d2f21576fa2a033f0ad631c3
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-06 15:35:02 +0200

    fixed opacity

--- client/src/components/SquareImage.jsx
+++ client/src/components/SquareImage.jsx
@@ -4,14 +4,14 @@ import { PiSpinnerGapLight } from "react-icons/pi";
 import { CgSpinner } from "react-icons/cg";
 
 
-function SquareImage({src, opacity}) {
+function SquareImage({src, title, opacity}) {
     const [loading, setLoading] = useState(true)
 
     const handleLoad = () => {
         setLoading(false)
     }
   return (
-    <div className={`relative flex w-full h-full aspect-square rounded-xl opacity-${opacity} transition-all duration-300 hover:opacity-100 cursor-pointer`}>
+    <div className={`relative flex w-full h-full rounded-xl opacity-50 transition-all duration-300 hover:opacity-100 cursor-pointer`} title={title}>
         {loading && <div className='w-full h-full absolute bg-body-color ring-2 ring-body-color flex justify-center items-center rounded-xl '><CgSpinner className='animate-spinLoader text-text-color-light/90 text-[40px] border-2 border-body-color' /></div> }
         <img src={src} className='h-full w-full object-cover object-top rounded-xl' onLoad={handleLoad} />
     </div>
