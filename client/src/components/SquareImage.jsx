796c8eded3aae35e629fdc403e01715162b7be5f
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-06 09:59:19 +0200

    square image component fiting to parent

--- client/src/components/SquareImage.jsx
+++ client/src/components/SquareImage.jsx
@@ -11,8 +11,8 @@ function SquareImage({src, opacity}) {
         setLoading(false)
     }
   return (
-    <div className={`relative flex w-[160px] h-[160px] min-w-[160px] min-h-[160px] aspect-square rounded-xl overflow-clip opacity-${opacity} transition-all duration-300 hover:opacity-100 cursor-pointer`}>
-        {loading && <div className='w-full h-full absolute bg-body-color border-2 border-body-color flex justify-center items-center rounded-xl '><CgSpinner className='animate-spinLoader text-text-color-light/90 text-[40px] border-2 border-body-color' /></div> }
+    <div className={`relative flex w-full h-full aspect-square rounded-xl opacity-${opacity} transition-all duration-300 hover:opacity-100 cursor-pointer`}>
+        {loading && <div className='w-full h-full absolute bg-body-color ring-2 ring-body-color flex justify-center items-center rounded-xl '><CgSpinner className='animate-spinLoader text-text-color-light/90 text-[40px] border-2 border-body-color' /></div> }
         <img src={src} className='h-full w-full object-cover object-top rounded-xl' onLoad={handleLoad} />
     </div>
   )
