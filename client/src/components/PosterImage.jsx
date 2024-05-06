ac70c7e03bf08ee4f358c8985ab736164b902a08
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-06 18:21:03 +0200

    image loader changed to  logo

--- client/src/components/PosterImage.jsx
+++ client/src/components/PosterImage.jsx
@@ -2,18 +2,23 @@ import React, { useState } from 'react'
 import { TbLoader2 } from "react-icons/tb";
 import { PiSpinnerGapLight } from "react-icons/pi";
 import { CgSpinner } from "react-icons/cg";
+import logo48 from '../assets/logo48.png'
 
 
-function PosterImage({src, title}) {
-    const [loading, setLoading] = useState(true)
+function PosterImage({ src, title }) {
+  const [loading, setLoading] = useState(true)
 
-    const handleLoad = () => {
-        setLoading(false)
-    }
+  const handleLoad = () => {
+    setLoading(false)
+  }
   return (
-    <div className={`relative flex w-full h-full rounded-xl opacity-80 transition-all duration-300 hover:opacity-100 cursor-pointer group`} title={title}>
-        {loading && <div className='w-full h-full absolute bg-body-color ring-2 ring-body-color flex justify-center items-center rounded-xl '><CgSpinner className='animate-spinLoader text-text-color-light/90 text-[40px] border-2 border-body-color' /></div> }
-        <img src={src} className='h-full w-full object-cover object-top rounded-xl transition group-hover:scale-[1.03] ' onLoad={handleLoad} />
+    <div className={`relative flex w-full h-full rounded-xl opacity-90 transition-all duration-300 hover:opacity-100 cursor-pointer group`} title={title}>
+      {loading &&
+        <div className='w-full h-full absolute dark:bg-body-color bg-white ring-2 ring-white dark:ring-body-color flex justify-center items-center rounded-xl '>
+          {/* <CgSpinner className='animate-spinLoader text-text-color-light/90 text-[40px]' /> */}
+          <img src={logo48} className='h-9 animate-ping' />
+        </div>}
+      <img src={src} className='h-full w-full object-cover object-top rounded-xl transition group-hover:scale-[1.03] ' onLoad={handleLoad} />
     </div>
   )
 }
--- client/src/components/SquareImage.jsx
+++ client/src/components/SquareImage.jsx
@@ -2,18 +2,23 @@ import React, { useState } from 'react'
 import { TbLoader2 } from "react-icons/tb";
 import { PiSpinnerGapLight } from "react-icons/pi";
 import { CgSpinner } from "react-icons/cg";
+import logo48 from '../assets/logo48.png'
 
 
-function SquareImage({src, title, opacity}) {
-    const [loading, setLoading] = useState(true)
+function SquareImage({ src, title, opacity }) {
+  const [loading, setLoading] = useState(true)
 
-    const handleLoad = () => {
-        setLoading(false)
-    }
+  const handleLoad = () => {
+    setLoading(false)
+  }
   return (
-    <div className={`relative flex w-full h-full rounded-xl dark:opacity-50 opacity-85 transition-all duration-300 hover:opacity-100 cursor-pointer`} title={title}>
-        {loading && <div className='w-full h-full absolute dark:bg-body-color bg-white ring-2 ring-white dark:ring-body-color flex justify-center items-center rounded-xl '><CgSpinner className='animate-spinLoader text-text-color-light/90 text-[40px]' /></div> }
-        <img src={src} className='h-full w-full object-cover object-top rounded-xl' onLoad={handleLoad} />
+    <div className={`relative flex w-full h-full rounded-xl dark:opacity-50 opacity-85 transition-all duration-300 hover:opacity-100 cursor-pointer group`} title={title}>
+      {loading && 
+      <div className='w-full h-full absolute dark:bg-body-color bg-white ring-2 ring-white dark:ring-body-color flex justify-center items-center rounded-xl '>
+        {/* <CgSpinner className='animate-spinLoader text-text-color-light/90 text-[40px]' /> */}
+        <img src={logo48} className='h-5 animate-ping' />
+      </div>}
+      <img src={src} className='h-full w-full object-cover object-top rounded-xl group-hover:scale-[1.03] transition' onLoad={handleLoad} />
     </div>
   )
 }
