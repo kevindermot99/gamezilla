5ad78235c0af5bbddfb184bd016342609b2e3b27
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-09 19:37:30 +0200

    new window browse

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -40,6 +40,7 @@ function Home() {
   const [modal, setModal] = useState(false);
   const [OpenedModalId, setOpendModalId] = useState("");
   const [muted, setMuted] = useState(true);
+  const [activeWindow, setActiveWindow] = useState('discover')
 
   // checking logged in user
   useEffect(() => {
@@ -50,6 +51,9 @@ function Home() {
       setUserId("none");
     }
   }, []);
+
+  const sortedBydowns = Games.sort((a, b) => b.downloads - a.downloads);
+  const top5 = sortedBydowns.slice(0, 5);
 
   const sortedGames = Games.sort(() => Math.random() - 0.5);
   // const swiper = new Swiper('.swiper', {
@@ -88,163 +92,198 @@ function Home() {
 
   return (
     <div
-      className={` relative h-svh flex flex-col overflow-x-clip text-text-color  `}
+      className={` relative h-fit max-sm:h-svh flex flex-col overflow-x-clip text-text-color  `}
     >
       {/* <div className="hero absolute top-0 left-0 w-full h-full -z-10 select-none pointer-events-none opacity-[0] "></div> */}
       <Menu userId={userId} />
 
-      {/* hero */}
-      <div className="w-full max-w-[1100px] px-5 mx-auto h-fit  flex items-start justify-start gap-3 mt-12">
-        <div className="w-3/4 min-h-full h-fit  ">
-          <Swiper spaceBetween={0} className="h-[400px] w-full flex relative ">
-            {Games.map((game, index) => (
-              <SwiperSlide
-                key={index}
-                className="h-full w-full flex overflow-clip  relative "
-              >
-                <img
-                  src={game.banner}
-                  className="w-full h-full object-cover"
-                  alt=""
-                />
-                <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-body-color/90 to-transparent flex flex-col items-start justify-end p-8 cursor-default ">
-                  <h1 className="text-[37px] leading-[40px] font-bold font-montserrat tracking-tight capitalize line-clamp-2 max-w-[80%] text-white">
-                    {game.title}
-                  </h1>
-                  <p className="text-[15px] leading-[20px] font-medium capitalize line-clamp-2 max-w-[70%] text-text-color pt-[6px]">
-                    {game.description}
-                  </p>
-                  <div className="w-full h-fit flex items-center justify-between pt-5">
-                    <Link
-                      to={`/`}
-                      className="bg-text-color/10 hover:bg-text-color/20 text-white font-semibold tracking-wider text-sm  py-[12px] px-12 rounded-xl"
-                    >
-                      GET
-                    </Link>
-                    <div className="flex items-center justify-end gap-3 bg-transparen py-3 px-4 rounded-xl">
-                      <span className="flex items-center justify-center text-xs">
-                        2008
-                      </span>
-                      <span className="flex items-center justify-center text-xs">
-                        &#x2022;
-                      </span>
-                      <span className="flex items-center justify-center text-xs">
-                        <LiaDownloadSolid className="text-sm" /> {numberFormat(game.downloads)}
-                      </span>
-                      <span className="flex items-center justify-center text-xs">
-                        &#x2022;
-                      </span>
-                      <span className="flex items-center justify-center gap-1 text-xs">
-                        <FaRegEye className="text-sm" /> 234K
-                      </span>
+      {/* mini Navbar */}
+      <div className="h-fit w-full max-w-[1100px] p-5 mx-auto sticky top-[-5px] z-30 bg-body-color flex items-center justify-start gap-2 ">
+        <form className="mr-3">
+          <input
+            type="search"
+            name="search"
+            className="bg-stone-100 placeholder:text-text-color-light/80 dark:bg-container-color/60 px-4 py-3 font-medium text-sm tracking-tight w-full max-w-[260px] "
+            autoComplete="on"
+            placeholder="Search Game"
+          />
+        </form>
+        <button
+          className={`px-2 py-2 text-[15px] font-DMsans font-medium tracking-tight ${activeWindow === 'discover' ? 'text-white' : 'text-text-color-light' }`}
+        >
+          Discover
+        </button>
+        <button
+          className={`px-2 py-2 text-[15px] font-DMsans font-medium tracking-tight ${activeWindow === 'discover' ? 'text-text-color-light' : 'text-white' }`}
+        >
+          Browse
+        </button>
+      </div>
+
+      {/* Discover */}
+      <div className={`w-full h-fit ${activeWindow === 'discover' ? '' : 'hidden' }`}>
+        {/* hero */}
+        <div className="w-full max-w-[1100px] px-5 mx-auto h-fit  flex items-start justify-start gap-3 ">
+          <div className="w-3/4 min-h-full h-fit  ">
+            <Swiper
+              spaceBetween={0}
+              className="h-[400px] w-full flex relative "
+            >
+              {top5.map((game, index) => (
+                <SwiperSlide
+                  key={index}
+                  className="h-full w-full flex overflow-clip  relative "
+                >
+                  <img
+                    src={game.banner}
+                    className="w-full h-full object-cover"
+                    alt=""
+                  />
+                  <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-body-color/90 to-transparent flex flex-col items-start justify-end p-8 cursor-default ">
+                    <h1 className="text-[37px] leading-[40px] font-bold font-montserrat tracking-tight capitalize line-clamp-2 max-w-[80%] text-white">
+                      {game.title}
+                    </h1>
+                    <p className="text-[15px] leading-[20px] font-medium capitalize line-clamp-2 max-w-[70%] text-text-color pt-[6px]">
+                      {game.description}
+                    </p>
+                    <div className="w-full h-fit flex items-center justify-between pt-5">
+                      <Link
+                        to={`/`}
+                        className="bg-text-color/10 hover:bg-text-color/20 text-white font-semibold tracking-wider text-sm  py-[12px] px-12 rounded-xl"
+                      >
+                        GET
+                      </Link>
+                      <div className="flex items-center justify-end gap-3 bg-transparen py-3 px-4 rounded-xl">
+                        <span className="flex items-center justify-center text-xs">
+                          2008
+                        </span>
+                        <span className="flex items-center justify-center text-xs">
+                          &#x2022;
+                        </span>
+                        <span className="flex items-center justify-center text-xs">
+                          <LiaDownloadSolid className="text-sm" />{" "}
+                          {numberFormat(game.downloads)}
+                        </span>
+                        <span className="flex items-center justify-center text-xs">
+                          &#x2022;
+                        </span>
+                        <span className="flex items-center justify-center gap-1 text-xs">
+                          <FaRegEye className="text-sm" /> 234K
+                        </span>
+                      </div>
                     </div>
                   </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <div className="w-1/4 min-w-[230px] flex flex-col gap-3 h-[400px] overflow-y-scroll snap-y snap-mandatory pr-3 overscroll-contain ">
+            {Games.map((game, index) => (
+              <div
+                key={index}
+                className="min-h-[60px] py-3 w-full snap-start flex items-center gap-2 overflow-clip relative cursor-pointer hover:bg-stone-100 dark:hover:bg-container-color/90 "
+              >
+                <div className=" h-[60px] min-w-[50px] overflow-clip flex">
+                  <img
+                    src={game.poster}
+                    className="h-fit min-h-[100%] min-w-[100%] w-[50px]  "
+                    alt=""
+                  />
                 </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-        <div className="w-1/4 min-w-[230px] flex flex-col gap-3 h-[400px] overflow-y-scroll snap-y snap-mandatory pr-3 overscroll-contain ">
-          {Games.map((game, index) => (
-            <div
-              key={index}
-              className="min-h-[60px] py-3 w-full snap-start flex items-center gap-2 overflow-clip relative cursor-pointer hover:bg-stone-100 dark:hover:bg-container-color/90 "
-            >
-              <div className=" h-[60px] min-w-[50px] overflow-clip flex">
-                <img
-                  src={game.poster}
-                  className="h-fit min-h-[100%] min-w-[100%] w-[50px]  "
-                  alt=""
-                />
+                <h1 className="line-clamp-2 text-sm text-black dark:text-text-color ">
+                  {game.title}
+                </h1>
               </div>
-              <h1 className="line-clamp-2 text-sm text-black dark:text-text-color ">{game.title}</h1>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
 
-      {/* most downloaded */}
-      <div className="w-full h-fit pt-7">
-        <div className="w-full h-fit flex flex-col max-w-[1100px] px-5 mx-auto">
-          <h1 className="text-lg capitalize font-DMsans font-medium w-full text-left tracking-tight  text-body-color dark:text-text-color pb-5">
-            For You
-          </h1>
-          <div className="h-fit ">
-            <div className="gridRespo ">
-              {Games.slice(0, 10).map((game, index) => (
-                <Link
-                  to={`/game/${game.id}`}
-                  key={index}
-                  className="group h-full w-full rounded-lg cursor-pointer mb-2 relative"
-                >
-                  <div className=" ">
-                    <PosterImage src={game.poster} title={game.title} />
-                  </div>
-                  <p className="font-normal text-[10px] line-clamp-2  text-body-color/90 dark:text-text-color-light max-w-[90%] pt-[10px] uppercase">
-                    Base Game
-                  </p>
-                  <p className="text-[16px] font-light capitalize line-clamp-1  text-black dark:text-text-color">
-                    {game.title}
-                  </p>
+        {/* most downloaded */}
+        <div className="w-full h-fit pt-7">
+          <div className="w-full h-fit flex flex-col max-w-[1100px] px-5 mx-auto">
+            <h1 className="text-lg capitalize font-DMsans font-medium w-full text-left tracking-tight  text-body-color dark:text-text-color pb-5">
+              For You
+            </h1>
+            <div className="h-fit ">
+              <div className="gridRespo ">
+                {Games.slice(0, 10).map((game, index) => (
+                  <Link
+                    to={`/game/${game.id}`}
+                    key={index}
+                    className="group h-full w-full rounded-lg cursor-pointer mb-2 relative"
+                  >
+                    <div className=" ">
+                      <PosterImage src={game.poster} title={game.title} />
+                    </div>
+                    <p className="font-normal text-[10px] line-clamp-2  text-body-color/90 dark:text-text-color-light max-w-[90%] pt-[10px] uppercase">
+                      Base Game
+                    </p>
+                    <p className="text-[16px] font-light capitalize line-clamp-1  text-black dark:text-text-color">
+                      {game.title}
+                    </p>
 
-                  <p className="font-montserrat mb-2 font-normal text-[14px] dark:text-text-color text-black flex items-center justify-start gap-2 pt-2 ">
-                    {/* <span className="flex items-center gap-1 font-montserrat dark:bg-main-color/50 bg-main-color text-white py-1 px-2 text-[9px] rounded-md">
+                    <p className="font-montserrat mb-2 font-normal text-[14px] dark:text-text-color text-black flex items-center justify-start gap-2 pt-2 ">
+                      {/* <span className="flex items-center gap-1 font-montserrat dark:bg-main-color/50 bg-main-color text-white py-1 px-2 text-[9px] rounded-md">
                       -100%
                     </span> */}
-                    <span className="flex items-center gap-2 text-sm">
-                      {/* <s></s> */}
-                      $0.00
-                    </span>
-                  </p>
-                  <button
-                    className={`absolute top-3 right-1 transition duration-300 opacity-0 group-hover:-translate-y-2 group-hover:opacity-100 bg-white dark:bg-body-color p-2 rounded-full`}
-                  >
-                    <BsCart className="text-xl " />
-                  </button>
-                </Link>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Our Collections */}
-      <div className="w-full h-fit pt-7">
-        <div className="w-full h-fit flex flex-col max-w-[1100px] px-5 mx-auto">
-          <h1 className="text-lg capitalize font-DMsans font-medium w-full text-left tracking-tight  text-body-color dark:text-text-color">
-            Our Collections
-          </h1>
-          <div className="h-fit mt-5 ">
-            <div className="gridRespoCollection ">
-              {Games.slice(0, 3).map((game, index) => (
-                <div
-                  to={`/game/${game.id}`}
-                  key={index}
-                  className="group h-full w-full rounded-lg mb-2 relative"
-                >
-                  <div className=" h-[160px] ">
-                    <CollectionImage src={game.banner} title={game.title} />
-                  </div>
-                  <p className="text-[16px] font-light capitalize line-clamp-1  text-body-color dark:text-text-color pt-3">
-                    {game.title}
-                  </p>
-                  <p className="font-normal text-sm line-clamp-2  text-body-color dark:text-text-color-light py-1">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Totam
-                  </p>
-
-                  <Link
-                    to={`/`}
-                    className="font-montserrat font-normal text-sm w-fit hover:underline text-text-color flex items-center justify-between pt-2 "
-                  >
-                    Discover
+                      <span className="flex items-center gap-2 text-sm">
+                        {/* <s></s> */}
+                        $0.00
+                      </span>
+                    </p>
+                    <button
+                      className={`absolute top-3 right-1 transition duration-300 opacity-0 group-hover:-translate-y-2 group-hover:opacity-100 bg-white dark:bg-body-color p-2 rounded-full`}
+                    >
+                      <BsCart className="text-xl " />
+                    </button>
                   </Link>
-                </div>
-              ))}
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Our Collections */}
+        <div className="w-full h-fit pt-7">
+          <div className="w-full h-fit flex flex-col max-w-[1100px] px-5 mx-auto">
+            <h1 className="text-lg capitalize font-DMsans font-medium w-full text-left tracking-tight  text-body-color dark:text-text-color">
+              Our Collections
+            </h1>
+            <div className="h-fit mt-5 ">
+              <div className="gridRespoCollection ">
+                {Games.slice(0, 3).map((game, index) => (
+                  <div
+                    to={`/game/${game.id}`}
+                    key={index}
+                    className="group h-full w-full rounded-lg mb-2 relative"
+                  >
+                    <div className=" h-[160px] ">
+                      <CollectionImage src={game.banner} title={game.title} />
+                    </div>
+                    <p className="text-[16px] font-light capitalize line-clamp-1  text-body-color dark:text-text-color pt-3">
+                      {game.title}
+                    </p>
+                    <p className="font-normal text-sm line-clamp-2  text-body-color dark:text-text-color-light py-1">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Totam
+                    </p>
+
+                    <Link
+                      to={`/`}
+                      className="font-montserrat font-normal text-sm w-fit hover:underline text-text-color flex items-center justify-between pt-2 "
+                    >
+                      Discover
+                    </Link>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+      {/* Browse */}
+      <div className={`bg-orange-400 h-[700px] ${activeWindow === 'browse' ? '' : 'hidden' }`}></div>
 
       <Footer />
     </div>
