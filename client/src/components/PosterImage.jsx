e0be787eaff1e059797c0c49c6f623572835f796
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-09 18:38:58 +0200

    not sharpen

--- client/src/components/PosterImage.jsx
+++ client/src/components/PosterImage.jsx
@@ -12,13 +12,13 @@ function PosterImage({ src, title }) {
     setLoading(false)
   }
   return (
-    <div className={`relative flex w-full h-full cursor-pointer group`} title={title}>
+    <div className={`relative flex w-full h-[220px] max-sm:h-[270px] overflow-clip cursor-pointer group `} title={title}>
       {loading &&
         <div className='w-full h-full absolute dark:bg-body-color bg-white ring-2 ring-white dark:ring-body-color flex justify-center items-center '>
           {/* <CgSpinner className='animate-spinLoader text-text-color-light/90 text-[40px]' /> */}
           <img src={logo48} className='h-9 animate-ping' />
         </div>}
-      <img src={src} className='h-full w-full object-cover object-top transition ' onLoad={handleLoad} />
+      <img src={src} className=' h-fit min-h-[100%] min-w-[100%] object-cover object-top transition ' onLoad={handleLoad} />
     </div>
   )
 }
