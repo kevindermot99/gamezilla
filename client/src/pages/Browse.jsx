47f9724ba49479c8b1620c83e5ad822b54cd849a
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-09 22:16:06 +0200

    browse reset scroll tot top

--- client/src/pages/Browse.jsx
+++ client/src/pages/Browse.jsx
@@ -2,38 +2,30 @@ import React, { useState, useEffect } from "react";
 import Menu from "../components/Menu";
 import MiniNavBar from "../components/MiniNavBar";
 import Footer from "../components/Footer";
+import { useLocation } from "react-router-dom";
 
 function Browse() {
-//   const [userId, setUserId] = useState("");
-
-//   // checking logged in user
-//   useEffect(() => {
-//     const userId = localStorage.getItem("gamezillaUserId");
-//     if (userId) {
-//       setUserId(userId);
-//     } else {
-//       setUserId("none");
-//     }
-//   }, []);
+  // resetting the scroll position
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <div
       className={` relative h-fit max-sm:h-svh flex flex-col overflow-x-clip text-text-color  `}
     >
-
       <div className="mb-2 w-full h-0"></div>
 
       {/* mini Navbar */}
       <MiniNavBar />
 
       {/* Discover */}
-      <div className="h-[500px] w-full">
-        browse
-      </div>
+      <div className="h-[500px] w-full">browse</div>
 
       <Footer />
     </div>
-  )
+  );
 }
 
 export default Browse;
