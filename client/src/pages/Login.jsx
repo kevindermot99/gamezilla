e910777296fb95d93b8cc25a530bb85ec04306e4
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-10 12:38:08 +0200

    login black and white

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,114 +1,161 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import { TbLoader2 } from "react-icons/tb";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Logo from "../assets/logo.png";
+import { MdOutlineKeyboardBackspace } from "react-icons/md";
 
 function Login() {
-    const [username, setUsername] = useState(null)
-    const [email, setEmail] = useState(null)
-    const [password, setPassword] = useState(null)
-    const [loginOut, setLoginOut] = useState(false)
-    const [authing, setAuthing] = useState(false)
+  const [username, setUsername] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [loginOut, setLoginOut] = useState(false);
+  const [authing, setAuthing] = useState(false);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const handleLogin = async (e) => {
-        e.preventDefault()
-        setAuthing(true)
-        try {
-            const response = await axios.post('http://localhost:3001/login', {
-                email,
-                password
-            })
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setAuthing(true);
+    try {
+      const response = await axios.post("http://localhost:3001/login", {
+        email,
+        password,
+      });
 
-            if (response.status === 200) {
-                console.log('Login successful')
-                localStorage.setItem('gamezillaUserId', response.data.userId)
-                localStorage.setItem('gamezillaUsername', response.data.username)
-                setAuthing(false)
-                setLoginOut(true)
-            }
-            else {
-                console.log('Login Failed')
-                setAuthing(false)
-            }
-        }
-        catch (error) {
-            if(error.response.status === 401){
-                navigate(`/vcode/${error.response.data.userId}`)
-                setAuthing(false)   
-            }
-            else{
-               console.log(error)
-            toast(error.response.data.message, {
-                // toastId: "customId"
-            }) 
-            }
-            
-        setAuthing(false)
+      if (response.status === 200) {
+        console.log("Login successful");
+        localStorage.setItem("gamezillaUserId", response.data.userId);
+        localStorage.setItem("gamezillaUsername", response.data.username);
+        setAuthing(false);
+        setLoginOut(true);
+      } else {
+        console.log("Login Failed");
+        setAuthing(false);
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        navigate(`/vcode/${error.response.data.userId}`);
+        setAuthing(false);
+      } else {
+        console.log(error);
+        toast(error.response.data.message, {
+          // toastId: "customId"
+        });
+      }
 
-        }
-
+      setAuthing(false);
     }
+  };
 
-    if (loginOut) {
-        navigate('/')
+  if (loginOut) {
+    navigate("/");
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  // checking logged in user
+  useEffect(() => {
+    const userId = localStorage.getItem("gamezillaUserId");
+    if (userId) {
+      navigate("/", { replace: true });
     }
+  }, []);
 
-    const handleEmail = (e) => {
-        setEmail(e.target.value)
-    }
-
-    const handlePassword = (e) => {
-        setPassword(e.target.value)
-    }
-
-    // checking logged in user
-    useEffect(() => {
-        const userId =  localStorage.getItem('gamezillaUserId')
-        if (userId) {
-            navigate('/', { replace: true })
-        }
-    },[])
-    
-
-    return (
-        <div className='px-[24px] py-5 flex flex-col h-full w-full bg-body-color '>
-            <ToastContainer
-                className='select-none'
-                position="bottom-left"
-                draggable
-                autoClose={3000}
-            />
-            <Link to={'/'} className='text-2xl font-bold flex items-center tracking-tight mr-8 w-fit'>Gamezilla.</Link>
-            <div className='h-full w-full flex items-center justify-center flex-col'>
-                <form onSubmit={handleLogin} className=' text-text-color w-full max-w-[390px] flex flex-col items-start justify-start '>
-                    
-                    <h1 className='text-3xl leading-[35px] font-bold '>Login</h1>
-                    <p className='mb-7 text-text-color/20 font-light text-sm pt-2'>Hi, Welcome Back</p>
-
-                    <label className='w-full flex flex-col mb-2'>
-                        <h1 className=' text-text-color/80 font-light mb-2 text-sm'>Email</h1>
-                        <input onChange={handleEmail} type="email" name='email' placeholder='someone@example.com' className='bg-border-line-color/60  py-3 px-5 w-full rounded-full placeholder:text-text-color-light/70 text-sm focus:ring-[2px] focus:ring-main-color  ' />
-                    </label>
-                    <label className='w-full flex flex-col mb-4'>
-                        <h1 className=' text-text-color/80 font-light mb-2 text-sm'>Password</h1>
-                        <input onChange={handlePassword} type="password" name='password' placeholder='Password' className='bg-border-line-color/60  py-3 px-5 w-full rounded-full placeholder:text-text-color-light/70 text-sm focus:ring-[2px] focus:ring-main-color   ' />
-                    </label>
-                    <a className='mb-5 text-main-color  font-light flex text-sm self-end'> Forgot Password?</a>
-                    <div className='w-full flex gap-2 mb-4 '>
-                        {/* <Link to="/" name='email' className=' w-full h-[40px] px-4 transition bg-border-line-color/60 hover:bg-border-line-color rounded-full text-sm flex items-center justify-center font-medium '>Cancel</Link> */}
-                        <button type="submit" name='email' className={` w-full h-[40px] px-4 transition bg-main-color hover:bg-main-color/60 rounded-full text-sm flex items-center justify-center font-medium ${authing && 'pointer-events-none '}`} >{authing ? <TbLoader2 className='animate-spinLoader text-white text-[25px] ' /> : 'Login'}</button>
-                    </div>
-                    <p className='text-sm font-light text-text-color/60 self-center mt-5'>Don't have an account? <Link to='/signup' className='text-main-color underline' > Sign up </Link></p>
-                </form>
-            </div>
+  return (
+    <div className="px-5 flex items-center justify-center min-h-[100vh]  w-full bg-white dark:bg-body-color pb-14">
+      <ToastContainer
+        className="select-none"
+        position="bottom-left"
+        draggable
+        autoClose={3000}
+      />
+      <div className="h-fit w-full max-w-[350px] flex items-start justify-center flex-col">
+        <Link
+          to={`/`}
+          className="text-black dark:text-text-color-light bg-stone-200 dark:bg-border-line-color/60 p-1 aspect-square rounded-full"
+        >
+          <MdOutlineKeyboardBackspace className="text-lg " />
+        </Link>
+        <div className="font-bold text-[30px] h-fit leading-[30px] tracking-tighter flex flex-col items-center justify-start w-full gap-0 mb-1 ">
+          <img src={Logo} className="h-16" />
+          Gamezilla
         </div>
-    )
+
+        <p className="mb-5 text-black/60 dark:text-text-color-light text-center font-medium text-sm w-full">
+          Login and let's pick up where we left off!
+        </p>
+
+        <form
+          onSubmit={handleLogin}
+          className=" text-black dark:text-text-color w-full flex flex-col items-start justify-start mt-3 "
+        >
+          <label className="w-full flex flex-col mb-3">
+            <h1 className=" text-black dark:text-text-color-light font-medium mb-2 text-sm">
+              Email
+            </h1>
+            <input
+              onChange={handleEmail}
+              type="email"
+              name="email"
+              placeholder="Someone@example.com"
+              className="bg-stone-200  dark:bg-border-line-color/60  py-3 px-4 w-full placeholder:text-text-color-light text-sm  "
+            />
+          </label>
+          <label className="w-full flex flex-col mb-3">
+            <h1 className=" text-black dark:text-text-color-light font-medium mb-2 text-sm">
+              Password
+            </h1>
+            <input
+              onChange={handlePassword}
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="bg-stone-200  dark:bg-border-line-color/60  py-3 px-4 w-full placeholder:text-text-color-light text-sm   "
+            />
+          </label>
+          <div className="w-full flex gap-2 mb-4 mt-2 ">
+            {/* <Link to="/" name='email' className=' w-full h-[40px] px-4 transition bg-border-line-color/60 hover:bg-border-line-color rounded-full text-sm flex items-center justify-center font-medium '>Cancel</Link> */}
+            <button
+              type="submit"
+              name="email"
+              className={` w-full h-[40px] px-4 transition hover:ring-2 ring-black dark:ring-white ring-offset-2 dark:ring-offset-body-color bg-black dark:bg-white text-white dark:text-black hover:opacity-85 text-sm flex items-center justify-center font-medium ${
+                authing && "pointer-events-none "
+              }`}
+            >
+              {authing ? (
+                <TbLoader2 className="animate-spinLoader text-white dark:text-black text-[25px] " />
+              ) : (
+                "Login"
+              )}
+            </button>
+          </div>
+          <a className=" text-black dark:text-white font-semibold underline flex text-sm self-center mt-3">
+            {" "}
+            Forgot Password?
+          </a>
+          <p className="text-sm font-light text-black dark:text-text-color-light self-center mt-2">
+            Don't have an account?{" "}
+            <Link
+              to="/signup"
+              className="text-black dark:text-white font-semibold underline"
+            >
+              {" "}
+              Sign up{" "}
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
