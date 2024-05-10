262cf68ea3513806904a16bc277f3e768d9d5493
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-10 11:05:37 +0200

    badge update

--- client/src/components/Badge.jsx
+++ client/src/components/Badge.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Badge = ({count}) => {
   
   return (
-    <div className=" absolute -top-[7px] -right-[15px] bg-main-color text-white aspect-square h-5 text-center pt-[1px] rounded-full text-[13px] font-bold font-mono ">
-        {count > 9 ? "9+" : count}
+    <div className=" absolute -top-[4px] -right-[12px] bg-black dark:bg-white text-white dark:text-black h-5 px-[6px] text-center pt-[1px] rounded-full text-[13px] font-bold font-mono ">
+        {count > 99 ? "99" : count}
     </div>
   )
 }
