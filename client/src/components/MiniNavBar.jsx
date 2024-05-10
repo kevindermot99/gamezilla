884fe3b760fb53904437bb123ffbbf3d7d1823f2
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-10 10:39:55 +0200

    mini nav re oder

--- client/src/components/MiniNavBar.jsx
+++ client/src/components/MiniNavBar.jsx
@@ -1,35 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function MiniNavBar() {
-    const location = useLocation()
+  const location = useLocation();
   return (
-    <div className='w-full h-fit bg-white dark:bg-body-color '>
-    <div className="h-fit w-full max-w-[1100px] px-5 py-3 mx-auto flex items-center justify-start gap-3 ">
-        <form className="">
+    <div className="w-full h-fit bg-white dark:bg-body-color ">
+      <div className="h-fit w-full max-w-[1100px] px-5 py-3 mx-auto flex items-center justify-between gap-3 ">
+        <div className="w-3/4 flex items-center gap-3 ">
+          <Link
+            to={`/`}
+            className={`px-3 h-[45px] flex items-center justify-center text-[15px] font-DMsans font-medium tracking-tight  ${
+              location.pathname === "/"
+                ? "text-black dark:text-white border-b-[2px] border-main-color "
+                : "text-black dark:text-text-color-light"
+            }`}
+          >
+            Discover
+          </Link>
+          <Link
+            to={`/browse`}
+            className={`px-3 h-[45px] flex items-center justify-center text-[15px] font-DMsans font-medium tracking-tight  ${
+              location.pathname === "/browse"
+                ? "text-black dark:text-white border-b-[2px] border-main-color "
+                : "text-black dark:text-text-color-light"
+            }`}
+          >
+            Browse
+          </Link>
+        </div>
+        <form className=" w-1/4">
           <input
             type="search"
             name="search"
-            className="bg-stone-100 hover:bg-stone-200/80 focus:bg-stone-200/80 dark:hover:bg-border-line-color/80 dark:focus:bg-border-line-color/80 text-black dark:text-white placeholder:text-text-color-light/80 dark:bg-container-color px-4 h-[45px] font-medium text-sm w-full max-w-[260px] "
+            className="h-[45px] w-full bg-stone-100 hover:bg-stone-200/50 focus:bg-stone-200/70 dark:hover:bg-border-line-color/80 dark:focus:bg-border-line-color/80 text-black dark:text-white placeholder:text-text-color-light/80 dark:bg-container-color px-4 font-medium text-sm  "
             autoComplete="off"
             placeholder="Search Game"
           />
         </form>
-        <Link to={`/`}
-          className={`px-3 h-[45px] flex items-center justify-center text-[15px] font-DMsans font-medium tracking-tight  ${location.pathname === '/' ? 'text-black dark:text-white border-b-[2px] border-main-color ' : 'text-black dark:text-text-color-light'}`}
-        >
-          Discover
-        </Link>
-        <Link to={`/browse`}
-        
-          className={`px-3 h-[45px] flex items-center justify-center text-[15px] font-DMsans font-medium tracking-tight  ${location.pathname === '/browse' ? 'text-black dark:text-white border-b-[2px] border-main-color ' : 'text-black dark:text-text-color-light'}`}
-        >
-          Browse
-        </Link>
       </div>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default MiniNavBar
+export default MiniNavBar;
