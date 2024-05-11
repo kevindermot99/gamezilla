f0144e569d026ab594e92571d215727d2af3346a
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-11 16:43:46 +0200

    menu account updated

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -131,7 +131,10 @@ function Menu() {
         </div>
 
         <div className={`h-full w-fit relative flex items-center gap-4`}>
-          <Link to={`/login`} className={` relative group flex px-1 gap-1  h-[35px] items-center`}>
+          <Link
+            to={`/login`}
+            className={` relative group flex px-1 gap-1  h-[35px] items-center`}
+          >
             <GoHeart className="text-xl dark:text-text-color text-black" />
             <Badge count={4} />
             <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-text-color/10 backdrop-blur-sm  text-white text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
@@ -139,7 +142,10 @@ function Menu() {
             </p>
           </Link>
 
-          <Link to={`/login`} className={` relative group flex px-1 gap-1 h-[35px] items-center `}>
+          <Link
+            to={`/login`}
+            className={` relative group flex px-1 gap-1 h-[35px] items-center `}
+          >
             <BsCart className="text-xl dark:text-text-color text-black" />
             <Badge count={24} />
             <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-text-color/10 backdrop-blur-sm  text-white text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
@@ -149,49 +155,26 @@ function Menu() {
 
           {userId === "none" ? (
             <Link to={`/login`} className={` relative group px-1 `}>
-            <div
-              className={`flex items-center justify-center gap-3 h-[35px] aspect-square rounded-full min-w-fit bg-stone-300 text-black dark:text-text-color dark:bg-stone-500/20  `}
-            >
-              <AiOutlineUser className="text-[18px]" />
-            </div>
+              <div
+                className={`flex items-center justify-center gap-3 h-[35px] aspect-square rounded-full min-w-fit bg-stone-300 text-black dark:text-text-color dark:bg-stone-500/20  `}
+              >
+                <AiOutlineUser className="text-[18px]" />
+              </div>
               <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-text-color/10 backdrop-blur-sm  text-white text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
                 Profile
               </p>
             </Link>
           ) : (
-            <div
-              className={`group flex items-center gap-2 h-full w-fit min-w-fit cursor-default relative ${
-                userId === "none" && "hidden"
-              }`}
-            >
-              <p className="dark:bg-container-color bg-stone-200 text-black dark:text-white h-8 aspect-square flex items-center justify-center capitalize font-mono font-bold text-2xl rounded-full  ">
-                {username.charAt(0)}
-              </p>
-              <p>{username}</p>
-              <div className="absolute top-[30px] pt-3 right-0 w-fit min-w-[200px] h-fit opacity-0 -z-50 pointer-events-none group-hover:opacity-100 group-hover:z-10 -translate-y-4 group-hover:translate-y-0 transition group-hover:pointer-events-auto   ">
-                <div className="bg-container-color bad rounded-lg p-2 w-full h-full">
-                  <button className="text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-main-color font-normal capitalize ">
-                    <RiAccountCircleLine className=" text-[25px] cursor-pointer " />{" "}
-                    Account
-                  </button>
-                  <button className="text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-main-color font-normal capitalize ">
-                    <MdOutlineDownloading className=" text-[25px] cursor-pointer " />{" "}
-                    Downloads
-                  </button>
-                  <button className="text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-main-color font-normal capitalize ">
-                    <HiCollection className=" text-[25px] cursor-pointer " />{" "}
-                    Collection
-                  </button>
-                  <div className="w-full h-[1px] bg-border-line-color/60 my-2"></div>
-                  <button
-                    onClick={handleLogout}
-                    className="text-sm px-2 h-[40px] rounded-lg hover:bg-stone-500/10 w-full text-left flex items-center justify-start gap-2 hover:text-red-500 font-normal capitalize "
-                  >
-                    <TbLogout2 className="text-[25px] cursor-pointer " /> Logout
-                  </button>
-                </div>
+            <Link to={`/account/${userId}`} className={` relative group px-1 `}>
+              <div
+                className={`flex items-center justify-center gap-3 h-[35px] aspect-square rounded-full min-w-fit bg-stone-300 text-black dark:text-text-color dark:bg-stone-500/20  `}
+              >
+                <AiOutlineUser className="text-[18px]" />
               </div>
-            </div>
+              <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-text-color/10 backdrop-blur-sm  text-white text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
+                {username}
+              </p>
+            </Link>
           )}
 
           <div className="w-fit h-[25px] border-l-[2px] border-l-stone-500/60"></div>
