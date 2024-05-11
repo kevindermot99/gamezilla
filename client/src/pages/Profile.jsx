f2aa92910c0a62a89ec5c9a47bdba9cbee7d0c10
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-11 18:29:15 +0200

    profile containers

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,8 +15,9 @@ function Profile({ userId }) {
     >
       <Menu />
 
-      <div>
-        
+      <div className=" w-full h-full flex justify-center items-start p-5 gap-5 max-w-[1100px] mx-auto">
+        <div className="w-1/3 h-fit min-h-[300px] bg-stone-100 dark:bg-container-color sticky top-20"></div>
+        <div className="w-2/3 h-fit min-h-[900px] bg-stone-100 dark:bg-container-color"></div>
       </div>
 
       {/* <Footer /> */}
