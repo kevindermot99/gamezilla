19769cc1db8f18ad786778902521983c607110e5
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-11 18:22:24 +0200

    profite the begining

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Menu from "../components/Menu";
+import MiniNavBar from "../components/MiniNavBar";
+import Footer from "../components/Footer";
+import { useLocation } from "react-router-dom";
 
-function Profile() {
+function Profile({ userId }) {
+  useEffect(() => {
+    document.title = "Profile";
+  }, []);
+
   return (
-    <div>Profile</div>
-  )
+    <div
+      className={` relative h-fit max-sm:h-svh flex flex-col gap-3 overflow-x-clip text-text-color  `}
+    >
+      <Menu />
+
+      <div>
+        
+      </div>
+
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
