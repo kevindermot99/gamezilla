bf06a9595859a277a30c7285fd115c975c352cf9
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-24 19:09:29 +0200

    logo upgrade fixed

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -188,141 +188,138 @@ function Menu({ cartCount, updateCount }) {
         </div>
       </div>
       <div
-        className={`sticky top-0 z-30 min-h-fit w-full px-[24px] py-9 h-[60px] bg-white dark:bg-body-color/90 dark:backdrop-blur-md text-body-color dark:text-text-color flex items-center gap-3 `}
+        className={`sticky top-0 z-30 min-h-fit w-full px-[24px] py-9 h-[60px] bg-white dark:bg-container-color text-body-color dark:text-text-color flex items-center gap-3 `}
       >
         <div className="w-full h-fit flex items-center justify-between gap-4">
-          {/* <div className="w-fit min-w-fit h-full flex items-center justify-start gap-4 ">
+          <div className="flex items-center gap-5 justify-start">
             <Link
               to="/"
-              className="font-bold text-2xl tracking-tighter flex items-center justify-center mr-3 gap-1 "
+              className="font-bold text-[27px] w-fit min-w-fit tracking-tighter flex items-center justify-center gap-2 "
             >
-              <img src={Logo} className="h-11" />
+              {/* <button><IoMdMenu /></button> */}
+              <img src={Logo} className="h-10" />
               Gamezilla
             </Link>
 
-            <Link
-              to={`/`}
-              className={`text-[15px] capitalize text-black dark:text-text-color/80 font-DMsans font-medium tracking-tight transition-colors h-full cursor-pointer py-2 px-1`}
-            >
-              All games
-            </Link>
-
-            <Link
-              to={`/`}
-              className={`text-[15px] capitalize text-black dark:text-text-color/80 font-DMsans font-medium tracking-tight transition-colors cursor-pointer py-2 px-1`}
-            >
-              collections
-            </Link>
-
-            <button
-              className={`group text-[15px] h-[60px] capitalize text-black dark:text-text-color/80 font-DMsans font-medium tracking-tight transition-colors cursor-pointer py-2 px-1 flex items-center gap-1`}
-            >
-              genres
-              <IoChevronDownSharp />
-              <div className="h-0 group-hover:h-[350px]  w-full opacity-100 group-hover:opacity-100 z-30 bg-white shadow dark:bg-container-color absolute top-[60px] left-0 right-0 mx-auto transition-all duration-200 cursor-default overflow-clip  ">
-                <div className="p-5 h-fit w-full max-w-[1200px] mx-auto flex items-start justify-center gap-[5%] ">
-                  {Genres.map((genre, index) => (
-                    <div
-                      to={`/`}
-                      key={index}
-                      className="flex flex-col items-start justify-start bg-orange"
-                    >
-                      <h1 className="text-sm text-black dark:text-white">
-                        {genre.section}
-                      </h1>
-                      <div className="flex flex-col items-start gap-3 justify-start mt-3 ">
-                        {genre.genres.map((gen, index) => (
-                          <Link
-                            to={`/`}
-                            key={index}
-                            className="text-border-line-color/80   dark:text-text-color/50 text-[1em] line-clamp-2 max-w-[160px] flex gap-2 text-left cursor-pointer hover:text-black dark:hover:text-white transition  "
-                          >
-                            <span>&#x2022;</span>
-                            {gen}
-                          </Link>
-                        ))}
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </button>
-
-            <Link
-              to={`/`}
-              className={`text-[15px] capitalize text-black dark:text-text-color/80 font-DMsans font-medium tracking-tight transition-colors cursor-pointer py-2 px-1`}
-            >
-              Support
-            </Link>
-          </div> */}
-          <Link
-            to="/"
-            className="font-bold text-[30px] w-fit min-w-fit tracking-tighter flex items-center justify-center mr-3 gap-2 "
-          >
-            {/* <button><IoMdMenu /></button> */}
-            {/* <img src={Logo} className="h-7" /> */}
-            Gamezilla
-          </Link>
-
-          <form className=" w-full max-w-[600px]">
-            <input
-              type="search"
-              name="search"
-              className="h-[39px] w-full bg-stone-100 dark:bg-gray-400/10  text-black dark:text-white placeholder:text-text-color-light  px-6 font-medium text-sm rounded-full  "
-              autoComplete="off"
-              placeholder="Search Game"
-            />
-          </form>
-
-          <div className={`h-full w-fit relative flex items-center gap-4`}>
-            <Link
-              to={`/login`}
-              className={` relative group flex px-1 gap-1  h-[35px] items-center`}
-            >
-              <GoHeart className="text-2xl dark:text-text-color text-black" />
-              <Badge count={0} />
-              <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-white backdrop-blur-sm text-white dark:text-black text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
-                Wishlist
-              </p>
-            </Link>
-
-            <button
-              onClick={showCartBar}
-              className={` relative group flex px-1 gap-1 h-[35px] items-center `}
-            >
-              <BsCart className="text-2xl dark:text-text-color text-black" />
-              <Badge count={cartCount} />
-              <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-white backdrop-blur-sm text-white dark:text-black text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
-                Cart
-              </p>
-            </button>
-
-            {userId === "none" ? (
-              <Link to={`/login`} className={` relative group px-1 `}>
-                <div
-                  className={`flex items-center justify-center gap-3 h-[32px] aspect-square rounded-full min-w-fit bg-transparent text-black dark:text-text-color ring-1 ring-black dark:ring-text-color font-Inter font-bold  text-[18px]  `}
-                >
-                  <AiOutlineUser className="text-[18px]" />
-                </div>
-                <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-white backdrop-blur-sm text-white dark:text-black text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
-                  Profile
-                </p>
-              </Link>
-            ) : (
+            <div className="w-fit min-w-fit h-full flex items-center justify-start gap-4 ">
               <Link
-                to={`/profile/${userId}`}
-                className={` relative group px-1 `}
+                to={`/`}
+                className={`text-[15px] capitalize text-black dark:text-text-color/80 font-DMsans font-medium tracking-tight transition-colors h-full cursor-pointer py-2 px-1`}
               >
-                <div
-                  className={`flex items-center justify-center gap-3 h-[32px] aspect-square rounded-full min-w-fit bg-transparent text-black dark:text-text-color ring-1 ring-black dark:ring-text-color font-Inter font-bold  text-[18px] `}
-                >
-                  {username.charAt(0)}
+                All games
+              </Link>
+
+              <Link
+                to={`/`}
+                className={`text-[15px] capitalize text-black dark:text-text-color/80 font-DMsans font-medium tracking-tight transition-colors cursor-pointer py-2 px-1`}
+              >
+                collections
+              </Link>
+
+              <button
+                className={`group text-[15px] h-[60px] capitalize text-black dark:text-text-color/80 font-DMsans font-medium tracking-tight transition-colors cursor-pointer py-2 px-1 flex items-center gap-1`}
+              >
+                genres
+                <IoChevronDownSharp />
+                <div className="h-0 group-hover:h-[350px]  w-full opacity-100 group-hover:opacity-100 z-30 bg-white shadow dark:bg-container-color absolute top-[65px] left-0 right-0 mx-auto transition-all duration-200 cursor-default overflow-clip  ">
+                  <div className="p-5 h-fit w-full max-w-[1200px] mx-auto flex items-start justify-center gap-[5%] ">
+                    {Genres.map((genre, index) => (
+                      <div
+                        to={`/`}
+                        key={index}
+                        className="flex flex-col items-start justify-start bg-orange"
+                      >
+                        <h1 className="text-sm text-black dark:text-white">
+                          {genre.section}
+                        </h1>
+                        <div className="flex flex-col items-start gap-3 justify-start mt-3 ">
+                          {genre.genres.map((gen, index) => (
+                            <Link
+                              to={`/`}
+                              key={index}
+                              className="text-border-line-color/80   dark:text-text-color/50 text-[1em] line-clamp-2 max-w-[160px] flex gap-2 text-left cursor-pointer hover:text-black dark:hover:text-white transition  "
+                            >
+                              <span>&#x2022;</span>
+                              {gen}
+                            </Link>
+                          ))}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
+              </button>
+
+              <Link
+                to={`/`}
+                className={`text-[15px] capitalize text-black dark:text-text-color/80 font-DMsans font-medium tracking-tight transition-colors cursor-pointer py-2 px-1`}
+              >
+                Support
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-5 justify-end">
+            <form className=" w-full max-w-[300px]">
+              <input
+                type="search"
+                name="search"
+                className="h-[39px] w-full bg-stone-100 dark:bg-gray-400/10  text-black dark:text-white placeholder:text-text-color-light  px-6 font-medium text-sm rounded-full  "
+                autoComplete="off"
+                placeholder="Search Game"
+              />
+            </form>
+
+            <div className={`h-full w-fit relative flex items-center gap-4 `}>
+              <Link
+                to={`/login`}
+                className={` relative group flex px-1 gap-1  h-[35px] items-center`}
+              >
+                <GoHeart className="text-2xl dark:text-text-color text-black" />
+                <Badge count={0} />
                 <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-white backdrop-blur-sm text-white dark:text-black text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
-                  {username}
+                  Wishlist
                 </p>
               </Link>
-            )}
+
+              <button
+                onClick={showCartBar}
+                className={` relative group flex px-1 gap-1 h-[35px] items-center `}
+              >
+                <BsCart className="text-2xl dark:text-text-color text-black" />
+                <Badge count={cartCount} />
+                <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-white backdrop-blur-sm text-white dark:text-black text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
+                  Cart
+                </p>
+              </button>
+
+              {userId === "none" ? (
+                <Link to={`/login`} className={` relative group px-1 `}>
+                  <div
+                    className={`flex items-center justify-center gap-3 h-[32px] aspect-square rounded-full min-w-fit bg-transparent text-black dark:text-text-color ring-1 ring-black dark:ring-text-color font-Inter font-bold  text-[18px]  `}
+                  >
+                    <AiOutlineUser className="text-[18px]" />
+                  </div>
+                  <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-white backdrop-blur-sm text-white dark:text-black text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
+                    Profile
+                  </p>
+                </Link>
+              ) : (
+                <Link
+                  to={`/profile/${userId}`}
+                  className={` relative group px-1 `}
+                >
+                  <div
+                    className={`flex items-center justify-center gap-3 h-[32px] aspect-square rounded-full min-w-fit bg-transparent text-black dark:text-text-color ring-1 ring-black dark:ring-text-color font-Inter font-bold  text-[18px] `}
+                  >
+                    {username.charAt(0)}
+                  </div>
+                  <p className="absolute top-[60px] group-hover:top-[65px] transition-all duration-150 z-40 left-[-50px] right-[-50px] mx-auto w-fit px-3 flex items-center bg-body-color dark:bg-white backdrop-blur-sm text-white dark:text-black text-sm py-1 font-montserrat font-medium opacity-0 translate-y-[-10px] pointer-events-none group-hover:opacity-100 ">
+                    {username}
+                  </p>
+                </Link>
+              )}
+            </div>
           </div>
         </div>
       </div>
