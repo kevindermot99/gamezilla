d1bfb19ad87781339aede37282247b5bd8aad066
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-24 18:08:44 +0200

    poster image loader image removed

--- client/src/components/PosterImage.jsx
+++ client/src/components/PosterImage.jsx
@@ -14,11 +14,10 @@ function PosterImage({ src, title }) {
   return (
     <div className={`relative flex w-full h-[220px] overflow-clip rounded-xl cursor-pointer group `} title={title}>
       {loading &&
-        <div className='w-full h-full absolute dark:bg-body-color bg-white ring-2 ring-white dark:ring-body-color flex justify-center items-center '>
-          {/* <CgSpinner className='animate-spinLoader text-text-color-light/90 text-[40px]' /> */}
-          <img src={logo48} className='h-9 animate-ping' />
+        <div className='w-full h-full absolute bg-gradient-to-br from-container-color to-body-color flex justify-center items-center animate-pulse '>
+          
         </div>}
-      <img src={src} className=' h-fit min-h-[100%] min-w-[100%] object-cover object-top transition ' onLoad={handleLoad} />
+      <img src={src} className={` h-fit min-h-[100%] min-w-[100%] object-cover object-top transition ${loading ? 'opacity-0' : ''} `} onLoad={handleLoad} />
     </div>
   )
 }
