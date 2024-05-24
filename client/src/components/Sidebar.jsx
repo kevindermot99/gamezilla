57fd9a7876653abd9303cb488f206713783c4416
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-24 18:53:23 +0200

    sidebar full links

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -23,10 +23,27 @@ function Sidebar() {
           Most Downloaded
         </Link>
         <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          Publishers
+          Wallpapers
+        </Link>
+        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
+          Terms of service
         </Link>
         <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
           FAQs
+        </Link>
+        
+
+        <span className={`text-lg capitalize font-DMsans font-medium w-full text-left tracking-tight py-2 mt-5 text-body-color dark:text-text-color `}>
+          Add Ons
+        </span>
+        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
+          Mods
+        </Link>
+        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
+          DLCs
+        </Link>
+        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
+          Patchs
         </Link>
         
 
