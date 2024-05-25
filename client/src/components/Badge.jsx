137510f471c57c1ab8b68c063e2830f1ab94d016
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-25 10:14:35 +0200

    badge main color

--- client/src/components/Badge.jsx
+++ client/src/components/Badge.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Badge = ({count}) => {
   
   return (
-    <div className=" absolute -top-[4px] -right-[12px] bg-black dark:bg-white text-white dark:text-black h-5 px-[6px] text-center pt-[1px] rounded-full text-[13px] font-bold font-mono ">
+    <div className=" absolute -top-[4px] -right-[12px] bg-main-color text-white h-5 px-[6px] text-center pt-[1px] rounded-full text-[13px] font-bold font-mono ">
         {count > 99 ? "99" : count}
     </div>
   )
