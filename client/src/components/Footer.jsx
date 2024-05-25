8476b491b16971e225231776be9835de8370e763
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-25 10:14:50 +0200

    footer mt

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='dark:bg-footer-color/40 bg-stone-100 h-[400px] w-full '>
+    <div className='dark:bg-footer-color/40 bg-stone-50 min-h-[500px] w-full mt-16 '>
       d
     </div>
   )
