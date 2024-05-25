8febed89db403aaad59211f0840a59478a3df891
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-25 08:36:43 +0200

    sidebar icons

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,54 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoCompass } from "react-icons/io5";
+import { HiViewGridAdd } from "react-icons/hi";
 
 function Sidebar() {
   return (
-    <div className='w-full h-full overflow-y-auto max-h-[80vh] flex flex-col justify-start items-start'>
-        <span className={`text-lg capitalize font-DMsans font-medium w-full text-left tracking-tight py-2 text-body-color dark:text-text-color `}>
-          Explore
-        </span>
-        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          All Games
-        </Link>
-        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          franchise
-        </Link>
-        <span className={`text-lg capitalize font-DMsans font-medium w-full text-left tracking-tight py-2 mt-5 text-body-color dark:text-text-color `}>
-          Quick access
-        </span>
-        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          Trending
-        </Link>
-        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          Most Downloaded
-        </Link>
-        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          Wallpapers
-        </Link>
-        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          Terms of service
-        </Link>
-        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          FAQs
-        </Link>
-        
+    <div className="w-full h-full overflow-y-auto max-h-[80vh] flex flex-col justify-start items-start">
+      <span
+        className={`flex items-center justify-start gap-1  text-lg capitalize font-DMsans font-medium w-full text-left tracking-tight py-2 text-body-color dark:text-text-color `}
+      >
+        <IoCompass className="text-[25px]" />
+        Explore
+      </span>
+      <Link
+        className={`flex items-center justify-start gap-1 capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-main-color dark:text-text-color-light transition dark:hover:text-white `}
+      >
+        Trending
+      </Link>
+      <Link
+        className={`flex items-center justify-start gap-1 capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-main-color dark:text-text-color-light transition dark:hover:text-white `}
+      >
+        Most Downloaded
+      </Link>
+      <Link
+        className={`flex items-center justify-start gap-1 capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-main-color dark:text-text-color-light transition dark:hover:text-white `}
+      >
+        Wallpapers
+      </Link>
+      <Link
+        className={`flex items-center justify-start gap-1 capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-main-color dark:text-text-color-light transition dark:hover:text-white `}
+      >
+        Terms of service
+      </Link>
+      <Link
+        className={`flex items-center justify-start gap-1 capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-main-color dark:text-text-color-light transition dark:hover:text-white `}
+      >
+        FAQs
+      </Link>
 
-        <span className={`text-lg capitalize font-DMsans font-medium w-full text-left tracking-tight py-2 mt-5 text-body-color dark:text-text-color `}>
-          Add Ons
-        </span>
-        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          Mods
-        </Link>
-        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          DLCs
-        </Link>
-        <Link className={` capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-black dark:text-text-color-light transition dark:hover:text-white `}>
-          Patchs
-        </Link>
-        
-
+      <span
+        className={`flex items-center justify-start gap-1  text-lg capitalize font-DMsans font-medium w-full text-left tracking-tight py-2 mt-5 text-body-color dark:text-text-color `}
+      >
+        <HiViewGridAdd className="text-[25px]" />
+        Add Ons
+      </span>
+      <Link
+        className={`flex items-center justify-start gap-1 capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-main-color dark:text-text-color-light transition dark:hover:text-white `}
+      >
+        Mods
+      </Link>
+      <Link
+        className={`flex items-center justify-start gap-1 capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-main-color dark:text-text-color-light transition dark:hover:text-white `}
+      >
+        DLCs
+      </Link>
+      <Link
+        className={`flex items-center justify-start gap-1 capitalize text-base py-2 font-medium font-DMsans tracking-tight w-full rounded-md text-black/70 hover:text-main-color dark:text-text-color-light transition dark:hover:text-white `}
+      >
+        Patchs
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
